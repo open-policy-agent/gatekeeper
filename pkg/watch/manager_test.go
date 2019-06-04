bb8a00a0eced7d04a618d31038e055a00c7acd0c
Craig Tabita <ctab@google.com>
2019-06-04 15:54:41 -0700

    Remove leftover manual started assignment
    Signed-off-by: Craig Tabita <ctab@google.com>

--- pkg/watch/manager_test.go
+++ pkg/watch/manager_test.go
@@ -303,7 +303,6 @@ func TestRegistrar(t *testing.T) {
 	}
 
 	t.Run("Single Add Waits For CRD Available", func(t *testing.T) {
-		wm.started = true
 		wm.newDiscovery = newDiscoveryFactory(true)
 		expectedAdded := newChange("FooCRD", reg)
 		added, removed, changed, err := wm.gatherChanges(wm.managedKinds.Get())
