d35e49a35b21002ab365839564d967ba36140407
Cameron McCarthy <camm@google.com>
2019-07-10 10:45:54 -0400

    Initial copy of the mutation controller
    Made a copy of the constraints controller, and changed all instances of
    "Constraint" to "Mutation." This means things don't compile yet, so
    commented out the implementation. Ensured that everything compiles.
    Actual changes will come in the following commits; this is just to make
    sure that future diffs will be easier to read.
    
    Signed-off-by: Cameron McCarthy <camm@google.com>

---
+++ pkg/controller/mutationtemplate/mutationtemplate_controller.go
@@ -0,0 +1,343 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package mutationtemplate
+
+import (
+	//"context"
+	//"fmt"
+	//"reflect"
+
+	"github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1alpha1"
+	opa "github.com/open-policy-agent/frameworks/constraint/pkg/client"
+	"github.com/open-policy-agent/gatekeeper/pkg/controller/constraint"
+	//"github.com/open-policy-agent/gatekeeper/pkg/util"
+	"github.com/open-policy-agent/gatekeeper/pkg/watch"
+	//"github.com/open-policy-agent/opa/ast"
+	//errorpkg "github.com/pkg/errors"
+	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	//"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	//"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	finalizerName = "mutationtemplate.finalizers.gatekeeper.sh"
+	ctrlName      = "mutationtemplate-controller"
+)
+
+var log = logf.Log.WithName("controller").WithValues("kind", "MutationTemplate")
+
+type Adder struct {
+	Opa          opa.Client
+	WatchManager *watch.WatchManager
+}
+
+// Add creates a new MutationTemplate Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func (a *Adder) Add(mgr manager.Manager) error {
+	r, err := newReconciler(mgr, a.Opa, a.WatchManager)
+	if err != nil {
+		return err
+	}
+	return add(mgr, r)
+}
+
+func (a *Adder) InjectOpa(o opa.Client) {
+	a.Opa = o
+}
+
+func (a *Adder) InjectWatchManager(wm *watch.WatchManager) {
+	a.WatchManager = wm
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager, opa opa.Client, wm *watch.WatchManager) (reconcile.Reconciler, error) {
+	mutationAdder := constraint.Adder{Opa: opa}
+	w, err := wm.NewRegistrar(
+		ctrlName,
+		[]func(manager.Manager, schema.GroupVersionKind) error{mutationAdder.Add})
+	if err != nil {
+		return nil, err
+	}
+	return &ReconcileMutationTemplate{
+		Client:  mgr.GetClient(),
+		scheme:  mgr.GetScheme(),
+		opa:     opa,
+		watcher: w,
+	}, nil
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New(ctrlName, mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to MutationTemplate
+	err = c.Watch(&source.Kind{Type: &v1alpha1.MutationTemplate{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileMutationTemplate{}
+
+// ReconcileMutationTemplate reconciles a MutationTemplate object
+type ReconcileMutationTemplate struct {
+	client.Client
+	scheme  *runtime.Scheme
+	watcher *watch.Registrar
+	opa     opa.Client
+}
+
+// Reconcile reads that state of the cluster for a MutationTemplate object and makes changes based on the state read
+// and what is in the MutationTemplate.Spec
+// Automatically generate RBAC rules to allow the Controller to read and write CRDs
+// +kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=templates.gatekeeper.sh,resources=mutationtemplates,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=templates.gatekeeper.sh,resources=mutationtemplates/status,verbs=get;update;patch
+func (r *ReconcileMutationTemplate) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	return reconcile.Result{}, nil
+	/*
+	// Fetch the MutationTemplate instance
+	instance := &v1alpha1.MutationTemplate{}
+	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	status := util.GetCTHAStatus(instance)
+	status.Errors = nil
+	crd, err := r.opa.CreateCRD(context.Background(), instance)
+	if err != nil {
+		var createErr *v1alpha1.CreateCRDError
+		if parseErrs, ok := err.(ast.Errors); ok {
+			for i := 0; i < len(parseErrs); i++ {
+				createErr = &v1alpha1.CreateCRDError{Code: parseErrs[i].Code, Message: parseErrs[i].Message, Location: parseErrs[i].Location.String()}
+				status.Errors = append(status.Errors, createErr)
+			}
+		} else {
+			createErr = &v1alpha1.CreateCRDError{Code: "create_error", Message: err.Error()}
+			status.Errors = append(status.Errors, createErr)
+		}
+
+		util.SetCTHAStatus(instance, status)
+		if updateErr := r.Update(context.Background(), instance); updateErr != nil {
+			log.Error(updateErr, "update error")
+			return reconcile.Result{Requeue: true}, nil
+		}
+		return reconcile.Result{}, nil
+	}
+	util.SetCTHAStatus(instance, status)
+
+	name := crd.GetName()
+	namespace := crd.GetNamespace()
+	if instance.GetDeletionTimestamp().IsZero() {
+		// Check if the mutation already exists
+		found := &apiextensionsv1beta1.CustomResourceDefinition{}
+		err = r.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: namespace}, found)
+		if err != nil && errors.IsNotFound(err) {
+			return r.handleCreate(instance, crd)
+
+		} else if err != nil {
+			return reconcile.Result{}, err
+
+		} else {
+			return r.handleUpdate(instance, crd, found)
+		}
+
+	}
+	return r.handleDelete(instance, crd)
+	*/
+}
+
+func (r *ReconcileMutationTemplate) handleCreate(
+	instance *v1alpha1.MutationTemplate,
+	crd *apiextensionsv1beta1.CustomResourceDefinition) (reconcile.Result, error) {
+	return reconcile.Result{}, nil
+	/*
+	name := crd.GetName()
+	log := log.WithValues("name", name)
+	log.Info("creating mutation")
+	if !containsString(finalizerName, instance.GetFinalizers()) {
+		instance.SetFinalizers(append(instance.GetFinalizers(), finalizerName))
+		if err := r.Update(context.Background(), instance); err != nil {
+			log.Error(err, "update error", err)
+			return reconcile.Result{Requeue: true}, nil
+		}
+	}
+	log.Info("loading code into OPA")
+	if _, err := r.opa.AddTemplate(context.Background(), instance); err != nil {
+		updateErr := &v1alpha1.CreateCRDError{Code: "update_error", Message: fmt.Sprintf("Could not update CRD: %s", err)}
+		status := util.GetCTHAStatus(instance)
+		status.Errors = append(status.Errors, updateErr)
+		util.SetCTHAStatus(instance, status)
+		if err2 := r.Update(context.Background(), instance); err2 != nil {
+			err = errorpkg.Wrap(err, fmt.Sprintf("Could not update status: %s", err2))
+		}
+		return reconcile.Result{}, err
+	}
+	log.Info("adding to watcher registry")
+	if err := r.watcher.AddWatch(makeGvk(instance.Spec.CRD.Spec.Names.Kind)); err != nil {
+		return reconcile.Result{}, err
+	}
+	// To support HA deployments, only one pod should be able to create CRDs
+	log.Info("creating mutation CRD")
+	if err := r.Create(context.TODO(), crd); err != nil {
+		status := util.GetCTHAStatus(instance)
+		status.Errors = []*v1alpha1.CreateCRDError{}
+		createErr := &v1alpha1.CreateCRDError{Code: "create_error", Message: fmt.Sprintf("Could not create CRD: %s", err)}
+		status.Errors = append(status.Errors, createErr)
+		util.SetCTHAStatus(instance, status)
+		if err2 := r.Update(context.Background(), instance); err2 != nil {
+			err = errorpkg.Wrap(err, fmt.Sprintf("Could not update status: %s", err2))
+		}
+		return reconcile.Result{}, err
+	}
+	instance.Status.Created = true
+	if err := r.Update(context.Background(), instance); err != nil {
+		return reconcile.Result{Requeue: true}, nil
+	}
+	return reconcile.Result{}, nil
+	*/
+}
+
+func (r *ReconcileMutationTemplate) handleUpdate(
+	instance *v1alpha1.MutationTemplate,
+	crd, found *apiextensionsv1beta1.CustomResourceDefinition) (reconcile.Result, error) {
+	return reconcile.Result{}, nil
+	/*
+	// TODO: We may want to only check in code if it has changed. This is harder to do than it sounds
+	// because even if the hash hasn't changed, OPA may have been restarted and needs code re-loaded
+	// anyway. We should see if the OPA server is smart enough to look for changes on its own, otherwise
+	// this may be too expensive to do in large clusters
+	name := crd.GetName()
+	log := log.WithValues("name", instance.GetName(), "crdName", name)
+	log.Info("loading mutation code into OPA")
+	if _, err := r.opa.AddTemplate(context.Background(), instance); err != nil {
+		updateErr := &v1alpha1.CreateCRDError{Code: "update_error", Message: fmt.Sprintf("Could not update CRD: %s", err)}
+		status := util.GetCTHAStatus(instance)
+		status.Errors = append(status.Errors, updateErr)
+		util.SetCTHAStatus(instance, status)
+		if err2 := r.Update(context.Background(), instance); err2 != nil {
+			err = errorpkg.Wrap(err, fmt.Sprintf("Could not update status: %s", err2))
+		}
+		return reconcile.Result{}, err
+	}
+	log.Info("making sure mutation is in watcher registry")
+	if err := r.watcher.AddWatch(makeGvk(instance.Spec.CRD.Spec.Names.Kind)); err != nil {
+		log.Error(err, "error adding template to watch registry")
+		return reconcile.Result{}, err
+	}
+	if !reflect.DeepEqual(crd.Spec, found.Spec) {
+		log.Info("difference in spec found, updating")
+		found.Spec = crd.Spec
+		if err := r.Update(context.Background(), found); err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+	if err := r.Update(context.Background(), instance); err != nil {
+		log.Error(err, "update error", err)
+		return reconcile.Result{Requeue: true}, nil
+	}
+	return reconcile.Result{}, nil
+	*/
+}
+
+func (r *ReconcileMutationTemplate) handleDelete(
+	instance *v1alpha1.MutationTemplate,
+	crd *apiextensionsv1beta1.CustomResourceDefinition) (reconcile.Result, error) {
+	return reconcile.Result{}, nil
+	/*
+	name := crd.GetName()
+	namespace := crd.GetNamespace()
+	log := log.WithValues("name", instance.GetName(), "crdName", name)
+	if containsString(finalizerName, instance.GetFinalizers()) {
+		if err := r.Delete(context.Background(), crd); err != nil && !errors.IsNotFound(err) {
+			return reconcile.Result{}, err
+		}
+		found := &apiextensionsv1beta1.CustomResourceDefinition{}
+		if err := r.Get(context.Background(), types.NamespacedName{Name: name, Namespace: namespace}, found); err == nil {
+			log.Info("child mutation CRD has not yet been deleted, waiting")
+			// The following allows the controller to recover from a finalizer deadlock that occurs while
+			// the controller is offline
+			if err := r.watcher.AddWatch(makeGvk(instance.Spec.CRD.Spec.Names.Kind)); err != nil {
+				return reconcile.Result{}, err
+			}
+			return reconcile.Result{Requeue: true}, nil
+		} else if err != nil && !errors.IsNotFound(err) {
+			return reconcile.Result{}, err
+		}
+		log.Info("removing from watcher registry")
+		if err := r.watcher.RemoveWatch(makeGvk(instance.Spec.CRD.Spec.Names.Kind)); err != nil {
+			return reconcile.Result{}, err
+		}
+		if _, err := r.opa.RemoveTemplate(context.Background(), instance); err != nil {
+			return reconcile.Result{}, err
+		}
+		instance.SetFinalizers(removeString(finalizerName, instance.GetFinalizers()))
+		if err := r.Update(context.Background(), instance); err != nil {
+			return reconcile.Result{Requeue: true}, nil
+		}
+	}
+	return reconcile.Result{}, nil
+	*/
+}
+
+func makeGvk(kind string) schema.GroupVersionKind {
+	return schema.GroupVersionKind{
+		Group:   "mutations.gatekeeper.sh",
+		Version: "v1alpha1",
+		Kind:    kind,
+	}
+}
+
+func containsString(s string, items []string) bool {
+	for _, item := range items {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func removeString(s string, items []string) []string {
+	var rval []string
+	for _, item := range items {
+		if item != s {
+			rval = append(rval, item)
+		}
+	}
+	return rval
+}
