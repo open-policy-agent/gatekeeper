d2864d2a47802a27296c9b285120d6874b33542a
Craig Tabita <ctab@google.com>
2019-08-27 16:48:08 -0700

    modify tester to show path of tested file
    Signed-off-by: Craig Tabita <ctab@google.com>

--- library/pod-security-policy/test.sh
+++ library/pod-security-policy/test.sh
@@ -4,6 +4,7 @@ set -e
 for path in $PWD/*; do
     if [ -d $path ]
     then
+        echo $path
         cd $path
         docker run -v $path:/tests openpolicyagent/opa test /tests/src.rego /tests/src_test.rego
     fi
