f8856dd78367a0ed17049335b006b28263d12c9e
Craig Tabita <ctab@google.com>
2019-10-23 14:54:00 -0700

    Dynamically generate clusterrolebinding
    Signed-off-by: Craig Tabita <ctab@google.com>

--- hack/generate_chart_manifests.sh
+++ hack/generate_chart_manifests.sh
@@ -3,6 +3,11 @@
 CHART_PATH=chart/gatekeeper-operator
 CONFIG_PATH=../config
 
+# templates/ns.yaml update
+# echo "Updating templates/ns.yaml"
+# cp ${CONFIG_PATH}/manager/manager.yaml ${CHART_PATH}/ns/_ns.yaml
+# kustomize build ${CHART_PATH}/ns -o ${CHART_PATH}/templates/ns.yaml
+
 # templates/crds.configs.yaml update
 echo "Updating templates/crds.configs.yaml"
 awk 'FNR==1 && NR!=1 {print "---"}{print}' ${CONFIG_PATH}/crds/*.yaml > ${CHART_PATH}/crds.configs/_crds.yaml
@@ -35,19 +40,37 @@ rm ${CHART_PATH}/templates/_crds.constraints.yaml
 rm ${CHART_PATH}/crds.constraints/_crds.yaml
 rm ${CHART_PATH}/crds.constraints/_chart-metadata.yaml
 
-# templates/rbac.yaml update
-echo "Updating templates/rbac.yaml"
-cp ${CONFIG_PATH}/rbac/rbac_role.yaml ${CHART_PATH}/rbac/_rbac.yaml
+# templates/clusterrole.yaml update
+echo "Updating templates/clusterrole.yaml"
+cp ${CONFIG_PATH}/rbac/rbac_role.yaml ${CHART_PATH}/clusterrole/_clusterrole.yaml
 GATEKEEPER_OPERATOR_NAME="'{{ template \"gatekeeper-operator.fullname\" . }}'"
-sed -i "s/manager-role/${GATEKEEPER_OPERATOR_NAME}/g" ${CHART_PATH}/rbac/_rbac.yaml
-cp chart-metadata.yaml ${CHART_PATH}/rbac/_chart-metadata.yaml
-sed -i "s/METADATA_NAME_PLACEHOLDER/${GATEKEEPER_OPERATOR_NAME}/g" ${CHART_PATH}/rbac/_chart-metadata.yaml
-sed -i 's/API_VERSION_PLACEHOLDER/rbac.authorization.k8s.io\/v1/g' ${CHART_PATH}/rbac/_chart-metadata.yaml
-sed -i 's/KIND_PLACEHOLDER/ClusterRole/g' ${CHART_PATH}/rbac/_chart-metadata.yaml
-kustomize build ${CHART_PATH}/rbac -o ${CHART_PATH}/templates/_rbac.yaml
+sed -i "s/manager-role/${GATEKEEPER_OPERATOR_NAME}/g" ${CHART_PATH}/clusterrole/_clusterrole.yaml
+cp chart-metadata.yaml ${CHART_PATH}/clusterrole/_chart-metadata.yaml
+sed -i "s/METADATA_NAME_PLACEHOLDER/${GATEKEEPER_OPERATOR_NAME}/g" ${CHART_PATH}/clusterrole/_chart-metadata.yaml
+sed -i 's/API_VERSION_PLACEHOLDER/rbac.authorization.k8s.io\/v1/g' ${CHART_PATH}/clusterrole/_chart-metadata.yaml
+sed -i 's/KIND_PLACEHOLDER/ClusterRole/g' ${CHART_PATH}/clusterrole/_chart-metadata.yaml
+kustomize build ${CHART_PATH}/clusterrole -o ${CHART_PATH}/templates/_clusterrole.yaml
 echo '{{- if .Values.rbac.create }}' > ${CHART_PATH}/templates/clusterrole.yaml
-cat ${CHART_PATH}/templates/_rbac.yaml >> ${CHART_PATH}/templates/clusterrole.yaml
+cat ${CHART_PATH}/templates/_clusterrole.yaml >> ${CHART_PATH}/templates/clusterrole.yaml
 echo '{{- end }}' >> ${CHART_PATH}/templates/clusterrole.yaml
-rm ${CHART_PATH}/rbac/_rbac.yaml
-rm ${CHART_PATH}/rbac/_chart-metadata.yaml
-rm ${CHART_PATH}/templates/_rbac.yaml
+rm ${CHART_PATH}/clusterrole/_clusterrole.yaml
+rm ${CHART_PATH}/clusterrole/_chart-metadata.yaml
+rm ${CHART_PATH}/templates/_clusterrole.yaml
+
+# templates/clusterrolebinding.yaml
+echo "Updating templates/clusterrolebinding.yaml"
+cp ${CONFIG_PATH}/rbac/rbac_role_binding.yaml ${CHART_PATH}/clusterrolebinding/_clusterrolebinding.yaml
+GATEKEEPER_OPERATOR_ROLEBINDING_NAME="'{{ template \"gatekeeper-operator.fullname\" . }}binding'"
+sed -i "s/manager-rolebinding/${GATEKEEPER_OPERATOR_ROLEBINDING_NAME}/g" ${CHART_PATH}/clusterrolebinding/_clusterrolebinding.yaml
+sed -i "s/manager-role/${GATEKEEPER_OPERATOR_NAME}/g" ${CHART_PATH}/clusterrolebinding/_clusterrolebinding.yaml
+cp chart-metadata.yaml ${CHART_PATH}/clusterrolebinding/_chart-metadata.yaml
+sed -i "s/METADATA_NAME_PLACEHOLDER/${GATEKEEPER_OPERATOR_ROLEBINDING_NAME}/g" ${CHART_PATH}/clusterrolebinding/_chart-metadata.yaml
+sed -i 's/API_VERSION_PLACEHOLDER/rbac.authorization.k8s.io\/v1/g' ${CHART_PATH}/clusterrolebinding/_chart-metadata.yaml
+sed -i 's/KIND_PLACEHOLDER/ClusterRoleBinding/g' ${CHART_PATH}/clusterrolebinding/_chart-metadata.yaml
+kustomize build ${CHART_PATH}/clusterrolebinding -o ${CHART_PATH}/templates/_clusterrolebinding.yaml
+echo '{{- if .Values.rbac.create }}' > ${CHART_PATH}/templates/clusterrolebinding.yaml
+cat ${CHART_PATH}/templates/_clusterrolebinding.yaml >> ${CHART_PATH}/templates/clusterrolebinding.yaml
+echo '{{- end }}' >> ${CHART_PATH}/templates/clusterrolebinding.yaml
+rm ${CHART_PATH}/clusterrolebinding/_clusterrolebinding.yaml
+rm ${CHART_PATH}/clusterrolebinding/_chart-metadata.yaml
+rm ${CHART_PATH}/templates/_clusterrolebinding.yaml
