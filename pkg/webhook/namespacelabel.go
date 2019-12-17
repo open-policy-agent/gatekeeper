ed1f25bd973d86fbf2560ceba4f53c6d71588834
Max Smythe <smythe@google.com>
2019-12-16 21:40:59 -0800

    Namespace label webhook should fail hard
    Signed-off-by: Max Smythe <smythe@google.com>

--- pkg/webhook/namespacelabel.go
+++ pkg/webhook/namespacelabel.go
@@ -21,7 +21,7 @@ func init() {
 
 const ignoreLabel = "gatekeeper-ignore"
 
-// +kubebuilder:webhook:verbs=CREATE;UPDATE,path=/v1/admitlabel,mutating=false,failurePolicy=ignore,groups="",resources=namespaces,versions=*,name=check-ignore-label.gatekeeper.sh
+// +kubebuilder:webhook:verbs=CREATE;UPDATE,path=/v1/admitlabel,mutating=false,failurePolicy=fail,groups="",resources=namespaces,versions=*,name=check-ignore-label.gatekeeper.sh
 
 // AddLabelWebhook registers the label webhook server with the manager
 func AddLabelWebhook(mgr manager.Manager, _ *opa.Client) error {
