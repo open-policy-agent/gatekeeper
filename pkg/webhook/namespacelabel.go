f66fc5a08b788dce77a5e349e26de0c70d388809
Max Smythe <smythe@google.com>
2019-12-16 21:25:29 -0800

    Add a webhook to reject the gatekeeper-ignore label on non-GK namespaces
    Fixes #231
    
    Signed-off-by: Max Smythe <smythe@google.com>

---
+++ pkg/webhook/namespacelabel.go
@@ -0,0 +1,59 @@
+package webhook
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	opa "github.com/open-policy-agent/frameworks/constraint/pkg/client"
+	"github.com/open-policy-agent/gatekeeper/pkg/util"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	types "k8s.io/api/admission/v1beta1"
+)
+
+func init() {
+	AddToManagerFuncs = append(AddToManagerFuncs, AddLabelWebhook)
+}
+
+const ignoreLabel = "gatekeeper-ignore"
+
+// +kubebuilder:webhook:verbs=CREATE;UPDATE,path=/v1/admitlabel,mutating=false,failurePolicy=ignore,groups="",resources=namespaces,versions=*,name=check-ignore-label.gatekeeper.sh
+
+// AddLabelWebhook registers the label webhook server with the manager
+func AddLabelWebhook(mgr manager.Manager, _ *opa.Client) error {
+	wh := &admission.Webhook{Handler: &namespaceLabelHandler{}}
+	mgr.GetWebhookServer().Register("/v1/admitlabel", wh)
+	return nil
+}
+
+var _ admission.Handler = &namespaceLabelHandler{}
+
+type namespaceLabelHandler struct{}
+
+func (h *namespaceLabelHandler) Handle(ctx context.Context, req admission.Request) admission.Response {
+	if req.Operation == types.Delete {
+		return admission.Allowed("Delete is always allowed")
+	}
+	if req.AdmissionRequest.Kind.Group != "" || req.AdmissionRequest.Kind.Kind != "Namespace" {
+		return admission.Allowed("Not a namespace")
+	}
+	obj := &unstructured.Unstructured{}
+	if err := json.Unmarshal(req.Object.Raw, obj); err != nil {
+		r := admission.Denied(errors.Wrap(err, "while deserializing resource").Error())
+		r.Result.Code = http.StatusInternalServerError
+		return r
+	}
+	if obj.GetName() == util.GetNamespace() {
+		return admission.Allowed(fmt.Sprintf("Gatekeeper namespace is allowed to set the %s label", ignoreLabel))
+	}
+	for label := range obj.GetLabels() {
+		if label == ignoreLabel {
+			return admission.Denied(fmt.Sprintf("Only Gatekeeper's namespace can have the %s label", ignoreLabel))
+		}
+	}
+	return admission.Allowed(fmt.Sprintf("Namespace is not setting the %s label", ignoreLabel))
+}
---
+++ pkg/webhook/namespacelabel_test.go
@@ -0,0 +1,186 @@
+package webhook
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	types "k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func gvk(group, version, kind string) metav1.GroupVersionKind {
+	return metav1.GroupVersionKind{Group: group, Version: version, Kind: kind}
+}
+
+func TestAdmission(t *testing.T) {
+	tests := []struct{
+		name string
+		kind metav1.GroupVersionKind
+		obj runtime.Object
+		op types.Operation
+		expectAllowed bool
+	} {
+		{
+			name: "Wrong group",
+			kind: gvk("random", "v1", "Namespace"),
+			obj: &unstructured.Unstructured{},
+			op: types.Create,
+			expectAllowed: true,
+		},
+		{
+			name: "Wrong kind",
+			kind: gvk("", "v1", "Arbitrary"),
+			obj: &unstructured.Unstructured{},
+			op: types.Create,
+			expectAllowed: true,
+		},
+		{
+			name: "Bad Namespace create rejected",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "random-namespace",
+					Labels: map[string]string{ignoreLabel: "true"},
+				},
+			},
+			op: types.Create,
+			expectAllowed: false,
+		},
+		{
+			name: "Bad Namespace update rejected",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "random-namespace",
+					Labels: map[string]string{ignoreLabel: "true"},
+				},
+			},
+			op: types.Update,
+			expectAllowed: false,
+		},
+		{
+			name: "Bad Namespace delete allowed",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "random-namespace",
+					Labels: map[string]string{ignoreLabel: "true"},
+				},
+			},
+			op: types.Delete,
+			expectAllowed: true,
+		},
+		{
+			name: "Bad Namespace no label allowed",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "random-namespace",
+				},
+			},
+			op: types.Create,
+			expectAllowed: true,
+		},
+		{
+			name: "Bad Namespace irrelevant label allowed",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "random-namespace",
+					Labels: map[string]string{"some-label": "true"},
+				},
+			},
+			op: types.Update,
+			expectAllowed: true,
+		},
+		{
+			name: "Gatekeeper Namespace create allowed",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "gatekeeper-system",
+					Labels: map[string]string{ignoreLabel: "true"},
+				},
+			},
+			op: types.Create,
+			expectAllowed: true,
+		},
+		{
+			name: "Gatekeeper Namespace update allowed",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "gatekeeper-system",
+					Labels: map[string]string{ignoreLabel: "true"},
+				},
+			},
+			op: types.Update,
+			expectAllowed: true,
+		},
+		{
+			name: "Gatekeeper Namespace delete allowed",
+			kind: gvk("", "v1", "Namespace"),
+			obj: &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "gatekeeper-system",
+					Labels: map[string]string{ignoreLabel: "true"},
+				},
+			},
+			op: types.Delete,
+			expectAllowed: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := os.Setenv("POD_NAME", "gatekeeper-system"); err != nil {
+				t.Fatal(err)
+			}
+			gvk := tt.obj.GetObjectKind()
+			gvk.SetGroupVersionKind(schema.GroupVersionKind{Group: tt.kind.Group, Version: tt.kind.Version, Kind: tt.kind.Kind})
+			bytes, err := json.Marshal(tt.obj)
+			if err != nil {
+				t.Fatal(err)
+			}
+			req := admission.Request {
+				AdmissionRequest: admissionv1beta1.AdmissionRequest {
+					Kind: tt.kind,
+					Object: runtime.RawExtension{Raw: bytes},
+					Operation: tt.op,
+				},
+			}
+			handler := &namespaceLabelHandler{}
+			resp := handler.Handle(context.Background(), req)
+			if resp.Allowed != tt.expectAllowed {
+				t.Errorf("resp.Allowed = %v, expected %v. Reason: %s", resp.Allowed, tt.expectAllowed, resp.Result.Reason)
+			}
+		})
+	}
+}
+
+
+func TestBadSerialization(t *testing.T) {
+	if err := os.Setenv("POD_NAME", "gatekeeper-system"); err != nil {
+		t.Fatal(err)
+	}
+
+	req := admission.Request {
+		AdmissionRequest: admissionv1beta1.AdmissionRequest {
+			Kind: gvk("", "v1", "Namespace"),
+			Object: runtime.RawExtension{Raw: []byte("asdfadsfa  awdf+-=-=pasdf")},
+			Operation: types.Create,
+		},
+	}
+	handler := &namespaceLabelHandler{}
+	resp := handler.Handle(context.Background(), req)
+	if resp.Allowed {
+		t.Errorf("resp.Allowed = %v, expected false. Reason: %s", resp.Allowed, resp.Result.Reason)
+	}
+}
