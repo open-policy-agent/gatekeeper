d96ed9539f768576be54d3cb111b95d84eb969c1
Oren Shomron <shomron@gmail.com>
2020-03-05 10:23:55 -0500

    Upgrade to controller-runtime 0.5.0 and kubernetes 1.17.
    Closes #506.
    
    Signed-off-by: Oren Shomron <shomron@gmail.com>

--- vendor/sigs.k8s.io/controller-runtime/pkg/envtest/helper.go
+++ vendor/sigs.k8s.io/controller-runtime/pkg/envtest/helper.go
@@ -1,6 +1,11 @@
 package envtest
 
-import apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+import (
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+)
 
 // mergePaths merges two string slices containing paths.
 // This function makes no guarantees about order of the merged slice.
@@ -23,19 +28,53 @@ func mergePaths(s1, s2 []string) []string {
 
 // mergeCRDs merges two CRD slices using their names.
 // This function makes no guarantees about order of the merged slice.
-func mergeCRDs(s1, s2 []*apiextensionsv1beta1.CustomResourceDefinition) []*apiextensionsv1beta1.CustomResourceDefinition {
-	m := make(map[string]*apiextensionsv1beta1.CustomResourceDefinition)
-	for _, crd := range s1 {
-		m[crd.Name] = crd
+func mergeCRDs(s1, s2 []runtime.Object) []runtime.Object {
+	m := make(map[string]*unstructured.Unstructured)
+	for _, obj := range runtimeListToUnstructured(s1) {
+		m[obj.GetName()] = obj
 	}
-	for _, crd := range s2 {
-		m[crd.Name] = crd
+	for _, obj := range runtimeListToUnstructured(s2) {
+		m[obj.GetName()] = obj
 	}
-	merged := make([]*apiextensionsv1beta1.CustomResourceDefinition, len(m))
+	merged := make([]runtime.Object, len(m))
 	i := 0
-	for _, crd := range m {
-		merged[i] = crd
+	for _, obj := range m {
+		merged[i] = obj
 		i++
 	}
 	return merged
+}
+
+// existsUnstructured verify if a any item is common between two lists.
+func existsUnstructured(s1, s2 []*unstructured.Unstructured) bool {
+	for _, s1obj := range s1 {
+		for _, s2obj := range s2 {
+			if reflect.DeepEqual(s1obj, s2obj) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func runtimeListToUnstructured(l []runtime.Object) []*unstructured.Unstructured {
+	res := []*unstructured.Unstructured{}
+	for _, obj := range l {
+		m, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj.DeepCopyObject())
+		if err != nil {
+			continue
+		}
+		res = append(res, &unstructured.Unstructured{
+			Object: m,
+		})
+	}
+	return res
+}
+
+func unstructuredListToRuntime(l []*unstructured.Unstructured) []runtime.Object {
+	res := []runtime.Object{}
+	for _, obj := range l {
+		res = append(res, obj.DeepCopy())
+	}
+	return res
 }
