open-policy-agent/gatekeeper

commit dc285d805103ac5f9d7d873c653567ee6a51e8ee

Introduce circuit breaker into objectTracker (#683)
Introduces a circuit breaker into objectTracker which is tripped once
expectations have been met. When tripped, internal state tracking memory
can be freed and subsequent operations will not consume additional
memory in the tracker.

Closes #660

Signed-off-by: Oren Shomron <shomron@gmail.com>

Co-authored-by: Max Smythe <smythe@google.com>

pkg/readiness/object_tracker.go
@@ -43,13 +43,14 @@ type Expectations interface {
 // Expectations are satisfied by calls to Observe().
 // Once all expectations are satisfied, Satisfied() will begin returning true.
 type objectTracker struct {
-	mu        sync.RWMutex
-	gvk       schema.GroupVersionKind
-	cancelled objSet // expectations that have been cancelled
-	expect    objSet // unresolved expectations
-	seen      objSet // observations made before their expectations
-	satisfied objSet // tracked to avoid re-adding satisfied expectations and to support unsatisfied()
-	populated bool   // all expectations have been provided
+	mu           sync.RWMutex
+	gvk          schema.GroupVersionKind
+	cancelled    objSet // expectations that have been cancelled
+	expect       objSet // unresolved expectations
+	seen         objSet // observations made before their expectations
+	satisfied    objSet // tracked to avoid re-adding satisfied expectations and to support unsatisfied()
+	populated    bool   // all expectations have been provided
+	allSatisfied bool   // true once all expectations have been satified. Acts as a circuit-breaker.
 }
 
 func newObjTracker(gvk schema.GroupVersionKind) *objectTracker {
@@ -72,13 +73,9 @@ func (t *objectTracker) Expect(o runtime.Object) {
 		return
 	}
 
-	accessor, err := meta.Accessor(o)
-	if err != nil {
-		return
-	}
-
 	// Don't expect resources which are being terminated.
-	if !accessor.GetDeletionTimestamp().IsZero() {
+	accessor, err := meta.Accessor(o)
+	if err == nil && !accessor.GetDeletionTimestamp().IsZero() {
 		return
 	}
 
@@ -109,6 +106,12 @@ func (t *objectTracker) Expect(o runtime.Object) {
 func (t *objectTracker) CancelExpect(o runtime.Object) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
+
+	// Respect circuit-breaker.
+	if t.allSatisfied {
+		return
+	}
+
 	k, err := objKeyFromObject(o)
 	if err != nil {
 		log.Error(err, "skipping")
@@ -152,6 +155,11 @@ func (t *objectTracker) Observe(o runtime.Object) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
+	// Respect circuit-breaker.
+	if t.allSatisfied {
+		return
+	}
+
 	k, err := objKeyFromObject(o)
 	if err != nil {
 		log.Error(err, "skipping")
@@ -160,6 +168,11 @@ func (t *objectTracker) Observe(o runtime.Object) {
 
 	// Ignore cancelled expectations
 	if _, ok := t.cancelled[k]; ok {
+		return
+	}
+
+	// Ignore satisfied expectations
+	if _, ok := t.satisfied[k]; ok {
 		return
 	}
 
@@ -193,44 +206,58 @@ func (t *objectTracker) Populated() bool {
 // Expectations must be populated before the tracker can be considered satisfied.
 // Expectations are marked as populated by calling ExpectationsDone().
 func (t *objectTracker) Satisfied() bool {
-	satisfied, seenKeys := func() (bool, []objKey) {
+	satisfied, needMutate := func() (bool, bool) {
 		t.mu.RLock()
 		defer t.mu.RUnlock()
 
-		if !t.populated {
-			return false, nil
-		}
-
-		if len(t.expect) == 0 {
-			return true, nil
-		}
-
-		// Resolve any expectations where the observation preceded the expect request.
-		var keys []objKey
-		for k := range t.seen {
-			if _, ok := t.expect[k]; !ok {
-				continue
-			}
-			keys = append(keys, k)
-		}
-		return false, keys
+		// We'll only need the write lock in certain conditions:
+		//  1. We haven't tripped the circuit breaker
+		//  2. Expectations have been previously populated
+		//  3. We have expectations and observations - some of these may match can be resolved.
+		needMutate :=
+			!t.allSatisfied &&
+				t.populated &&
+				((len(t.seen) > 0 && len(t.expect) > 0) || // ...are there expectations we can resolve?
+					(len(t.seen) == 0 && len(t.expect) == 0)) // ...is the circuit-breaker ready to flip?
+		return t.allSatisfied, needMutate
 	}()
 
-	if len(seenKeys) == 0 {
-		return satisfied
+	if satisfied {
+		return true
+	}
+
+	// Proceed only if we have state changes to make.
+	if !needMutate {
+		return false
 	}
 
 	// From here we need a write lock to mutate state.
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
-	for _, k := range seenKeys {
+	// Resolve any expectations where the observation preceded the expect request.
+	for k := range t.seen {
+		if _, ok := t.expect[k]; !ok {
+			continue
+		}
 		delete(t.seen, k)
 		delete(t.expect, k)
 		t.satisfied[k] = struct{}{}
 	}
 
-	return len(t.expect) == 0
+	// All satisfied if:
+	//  1. Expectations have been previously populated
+	//  2. No expectations remain
+	if t.populated && len(t.expect) == 0 {
+		t.allSatisfied = true
+
+		// Circuit-breaker tripped - free tracking memory
+		t.seen = nil
+		t.expect = nil
+		t.satisfied = nil
+		t.cancelled = nil
+	}
+	return t.allSatisfied
 }
 
 func (t *objectTracker) kinds() []schema.GroupVersionKind {

pkg/readiness/object_tracker_test.go
@@ -91,7 +91,7 @@ func Test_ObjectTracker_Seen_Before_Expect(t *testing.T) {
 }
 
 // Verify that terminated resources are ignored when calling Expect.
-func Test_ObjectTracker_Termintated_Expect(t *testing.T) {
+func Test_ObjectTracker_Terminated_Expect(t *testing.T) {
 	g := gomega.NewWithT(t)
 	ot := newObjTracker(schema.GroupVersionKind{})
 	ct := makeCT("test-ct")
@@ -160,4 +160,59 @@ func Test_ObjectTracker_CancelBeforeExpect(t *testing.T) {
 	ot.ExpectationsDone()
 
 	g.Expect(ot.Satisfied()).To(gomega.BeTrue())
+}
+
+// Verify that the allSatisfied circuit breaker keeps Satisfied==true and
+// no other operations have any impact.
+func Test_ObjectTracker_CircuitBreaker(t *testing.T) {
+	g := gomega.NewWithT(t)
+	ot := newObjTracker(schema.GroupVersionKind{})
+
+	const count = 10
+	ct := makeCTSlice("ct-", count)
+	for i := 0; i < len(ct); i++ {
+		ot.Expect(ct[i])
+	}
+
+	g.Expect(ot.Satisfied()).NotTo(gomega.BeTrue(), "should not be satisfied before ExpectationsDone")
+	ot.ExpectationsDone()
+
+	for i := 0; i < len(ct); i++ {
+		ot.Observe(ct[i])
+	}
+
+	g.Expect(ot.Satisfied()).To(gomega.BeTrue())
+
+	// The circuit-breaker should have been tripped. Let's try different operations
+	// and make sure they do not consume additional memory or affect the satisfied state.
+	for i := 0; i < len(ct); i++ {
+		ot.CancelExpect(ct[i])
+		ot.Observe(ct[i])
+	}
+
+	expectNoObserve := makeCTSlice("expectNoObserve-", count)
+	for i := 0; i < len(expectNoObserve); i++ {
+		// Expect resources we won't then observe
+		ot.Expect(expectNoObserve[i])
+	}
+	cancelNoObserve := makeCTSlice("cancelNoObserve-", count)
+	for i := 0; i < len(cancelNoObserve); i++ {
+		// Cancel resources we won't then observe
+		ot.CancelExpect(cancelNoObserve[i])
+	}
+	justObserve := makeCTSlice("justObserve-", count)
+	for i := 0; i < len(justObserve); i++ {
+		// Observe some unexpected resources
+		ot.Observe(justObserve[i])
+	}
+
+	g.Expect(ot.Satisfied()).To(gomega.BeTrue())
+
+	// Peek at internals - we should no be accruing memory from the post-circuit-breaker operations
+	ot.mu.RLock()
+	defer ot.mu.RUnlock()
+	g.Expect(ot.cancelled).To(gomega.BeEmpty())
+	g.Expect(ot.expect).To(gomega.BeEmpty())
+	g.Expect(ot.seen).To(gomega.BeEmpty())
+	g.Expect(ot.satisfied).To(gomega.BeEmpty())
 }

commit 9a9e60a66b6f257d7d8acae4bc3bbc8d89f71f7e

Add verbose logging option to readiness tracker (#736)
* Add optional verbose logging to readiness tracker.

This commit adds optional verbose logs to help diagnose
unready pods. These are enabled using the new `statsEnabled`
flag of the Config resource:

```yaml
apiVersion: config.gatekeeper.sh/v1alpha1
kind: Config
metadata:
  name: config
  namespace: "gatekeeper-system"
spec:
  readiness:
    statsEnabled: true
```

Readiness logs are emitted on a 15 second interval only while the
tracker expectations have not yet been satisfied.

Example logs:

```
2020-07-21T12:17:12.669-0400    info    readiness-tracker       --- Begin unsatisfied data ---  {"gvk": "/v1, Kind=Namespace", "populated": true, "count": 6}
2020-07-21T12:17:12.669-0400    info    readiness-tracker       unsatisfied data        {"name": "/default", "gvk": "/v1, Kind=Namespace"}
2020-07-21T12:17:12.669-0400    info    readiness-tracker       unsatisfied data        {"name": "/gatekeeper-system", "gvk": "/v1, Kind=Namespace"}
2020-07-21T12:17:12.669-0400    info    readiness-tracker       unsatisfied data        {"name": "/kube-node-lease", "gvk": "/v1, Kind=Namespace"}
2020-07-21T12:17:12.669-0400    info    readiness-tracker       unsatisfied data        {"name": "/kube-public", "gvk": "/v1, Kind=Namespace"}
2020-07-21T12:17:12.669-0400    info    readiness-tracker       unsatisfied data        {"name": "/kube-system", "gvk": "/v1, Kind=Namespace"}
2020-07-21T12:17:12.669-0400    info    readiness-tracker       unsatisfied data        {"name": "/local-path-storage", "gvk": "/v1, Kind=Namespace"}
```

Fixes: #696

Signed-off-by: Oren Shomron <shomron@gmail.com>

* Snapshot objectTracker kinds when tripping circuit-breaker before releasing memory.

Fixes bug introduced in #683 which caused objectTracker.kinds() to clear
itself when the tracker's circuit breaker tripped, which would lead to
false readiness reporting on "subordinate" trackers that depended on

apis/config/v1alpha1/config_types.go
@@ -31,6 +31,9 @@ type ConfigSpec struct {
 
 	// Configuration for namespace exclusion
 	Match []MatchEntry `json:"match,omitempty"`
+
+	// Configuration for readiness tracker
+	Readiness ReadinessSpec `json:"readiness,omitempty"`
 }
 
 type Validation struct {
@@ -61,6 +64,10 @@ type SyncOnlyEntry struct {
 type MatchEntry struct {
 	ExcludedNamespaces []string `json:"excludedNamespaces,omitempty"`
 	Processes          []string `json:"processes,omitempty"`
+}
+
+type ReadinessSpec struct {
+	StatsEnabled bool `json:"statsEnabled,omitempty"`
 }
 
 // ConfigStatus defines the observed state of Config

apis/config/v1alpha1/zz_generated.deepcopy.go
@@ -94,6 +94,7 @@ func (in *ConfigSpec) DeepCopyInto(out *ConfigSpec) {
 			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
+	out.Readiness = in.Readiness
 }
 
 // DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ConfigSpec.
@@ -157,6 +158,21 @@ func (in *MatchEntry) DeepCopy() *MatchEntry {
 		return nil
 	}
 	out := new(MatchEntry)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ReadinessSpec) DeepCopyInto(out *ReadinessSpec) {
+	*out = *in
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ReadinessSpec.
+func (in *ReadinessSpec) DeepCopy() *ReadinessSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(ReadinessSpec)
 	in.DeepCopyInto(out)
 	return out
 }

pkg/controller/config/config_controller.go
@@ -221,6 +221,7 @@ func (r *ReconcileConfig) Reconcile(request reconcile.Request) (reconcile.Result
 
 	newSyncOnly := watch.NewSet()
 	newExcluder := process.New()
+	var statsEnabled bool
 	// If the config is being deleted the user is saying they don't want to
 	// sync anything
 	if exists && instance.GetDeletionTimestamp().IsZero() {
@@ -230,6 +231,16 @@ func (r *ReconcileConfig) Reconcile(request reconcile.Request) (reconcile.Result
 		}
 
 		newExcluder.Add(instance.Spec.Match)
+		statsEnabled = instance.Spec.Readiness.StatsEnabled
+	}
+
+	// Enable verbose readiness stats if requested.
+	if statsEnabled {
+		log.Info("enabling readiness stats")
+		r.tracker.EnableStats(ctx)
+	} else {
+		log.Info("disabling readiness stats")
+		r.tracker.DisableStats(ctx)
 	}
 
 	// Remove expectations for resources we no longer watch.

pkg/readiness/object_tracker.go
@@ -16,6 +16,7 @@ limitations under the License.
 package readiness
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1beta1"
@@ -43,14 +44,15 @@ type Expectations interface {
 // Expectations are satisfied by calls to Observe().
 // Once all expectations are satisfied, Satisfied() will begin returning true.
 type objectTracker struct {
-	mu           sync.RWMutex
-	gvk          schema.GroupVersionKind
-	cancelled    objSet // expectations that have been cancelled
-	expect       objSet // unresolved expectations
-	seen         objSet // observations made before their expectations
-	satisfied    objSet // tracked to avoid re-adding satisfied expectations and to support unsatisfied()
-	populated    bool   // all expectations have been provided
-	allSatisfied bool   // true once all expectations have been satified. Acts as a circuit-breaker.
+	mu            sync.RWMutex
+	gvk           schema.GroupVersionKind
+	cancelled     objSet                    // expectations that have been cancelled
+	expect        objSet                    // unresolved expectations
+	seen          objSet                    // observations made before their expectations
+	satisfied     objSet                    // tracked to avoid re-adding satisfied expectations and to support unsatisfied()
+	populated     bool                      // all expectations have been provided
+	allSatisfied  bool                      // true once all expectations have been satisfied. Acts as a circuit-breaker.
+	kindsSnapshot []schema.GroupVersionKind // Snapshot of kinds before freeing memory in Satisfied.
 }
 
 func newObjTracker(gvk schema.GroupVersionKind) *objectTracker {
@@ -228,6 +230,7 @@ func (t *objectTracker) Satisfied() bool {
 
 	// Proceed only if we have state changes to make.
 	if !needMutate {
+		log.V(1).Info("readiness state", "gvk", t.gvk, "satisfied", fmt.Sprintf("%d/%d", len(t.satisfied), len(t.expect)+len(t.satisfied)))
 		return false
 	}
 
@@ -236,6 +239,7 @@ func (t *objectTracker) Satisfied() bool {
 	defer t.mu.Unlock()
 
 	// Resolve any expectations where the observation preceded the expect request.
+	var resolveCount int
 	for k := range t.seen {
 		if _, ok := t.expect[k]; !ok {
 			continue
@@ -243,15 +247,20 @@ func (t *objectTracker) Satisfied() bool {
 		delete(t.seen, k)
 		delete(t.expect, k)
 		t.satisfied[k] = struct{}{}
+		resolveCount++
 	}
+	log.V(1).Info("resolved pre-observations", "gvk", t.gvk, "count", resolveCount)
+	log.V(1).Info("readiness state", "gvk", t.gvk, "satisfied", fmt.Sprintf("%d/%d", len(t.satisfied), len(t.expect)+len(t.satisfied)))
 
 	// All satisfied if:
 	//  1. Expectations have been previously populated
 	//  2. No expectations remain
 	if t.populated && len(t.expect) == 0 {
 		t.allSatisfied = true
+		log.V(1).Info("all expectations satisfied", "gvk", t.gvk)
 
 		// Circuit-breaker tripped - free tracking memory
+		t.kindsSnapshot = t.kindsNoLock() // Take snapshot as kinds() depends on the maps we're about to clear.
 		t.seen = nil
 		t.expect = nil
 		t.satisfied = nil
@@ -263,18 +272,31 @@ func (t *objectTracker) Satisfied() bool {
 func (t *objectTracker) kinds() []schema.GroupVersionKind {
 	t.mu.RLock()
 	defer t.mu.RUnlock()
+	return t.kindsNoLock()
+}
 
-	l := len(t.satisfied) + len(t.expect)
-	if l == 0 {
+func (t *objectTracker) kindsNoLock() []schema.GroupVersionKind {
+	if t.kindsSnapshot != nil {
+		out := make([]schema.GroupVersionKind, len(t.kindsSnapshot))
+		copy(out, t.kindsSnapshot)
+		return out
+	}
+
+	m := make(map[schema.GroupVersionKind]struct{})
+	for k := range t.satisfied {
+		m[k.gvk] = struct{}{}
+	}
+	for k := range t.expect {
+		m[k.gvk] = struct{}{}
+	}
+
+	if len(m) == 0 {
 		return nil
 	}
 
-	out := make([]schema.GroupVersionKind, 0, l)
-	for k := range t.satisfied {
-		out = append(out, k.gvk)
-	}
-	for k := range t.expect {
-		out = append(out, k.gvk)
+	out := make([]schema.GroupVersionKind, 0, len(m))
+	for k := range m {
+		out = append(out, k)
 	}
 	return out
 }
@@ -310,16 +332,19 @@ func objKeyFromObject(obj runtime.Object) (objKey, error) {
 	return objKey{namespacedName: nn, gvk: gvk}, nil
 }
 
-// this method is currently used only by tests
-// checks if objectTracker.expected contains the object with the gvk and namespaced name
-func (t *objectTracker) ExpectedContains(gvk schema.GroupVersionKind, nsName types.NamespacedName) bool {
+// IsExpecting returns true if the gvk/name combination was previously expected by the tracker.
+// Only valid until allSatisfied==true as tracking memory is freed at that point.
+// For testing only.
+func (t *objectTracker) IsExpecting(gvk schema.GroupVersionKind, nsName types.NamespacedName) bool {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 
-	for k := range t.expect {
-		if k.gvk.String() == gvk.String() && k.namespacedName.String() == nsName.String() {
-			return true
-		}
+	k := objKey{gvk: gvk, namespacedName: nsName}
+	if _, ok := t.expect[k]; ok {
+		return true
+	}
+	if _, ok := t.satisfied[k]; ok {
+		return true
 	}
 	return false
 }

pkg/readiness/ready_tracker.go
@@ -21,6 +21,7 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1beta1"
 	"github.com/open-policy-agent/frameworks/constraint/pkg/core/templates"
@@ -38,7 +39,10 @@ import (
 
 var log = logf.Log.WithName("readiness-tracker")
 
-const constraintGroup = "constraints.gatekeeper.sh"
+const (
+	constraintGroup = "constraints.gatekeeper.sh"
+	statsPeriod     = 15 * time.Second
+)
 
 // Lister lists resources from a cache.
 type Lister interface {
@@ -59,6 +63,7 @@ type Tracker struct {
 
 	ready              chan struct{}
 	constraintTrackers *syncutil.SingleRunner
+	statsEnabled       syncutil.SyncBool
 }
 
 func NewTracker(lister Lister) *Tracker {
@@ -147,6 +152,7 @@ func (t *Tracker) Satisfied(ctx context.Context) bool {
 			return false
 		}
 	}
+	log.V(1).Info("all expectations satisfied", "tracker", "constraints")
 
 	if !t.config.Satisfied() {
 		return false
@@ -157,6 +163,7 @@ func (t *Tracker) Satisfied(ctx context.Context) bool {
 			return false
 		}
 	}
+	log.V(1).Info("all expectations satisfied", "tracker", "data")
 
 	t.mu.Lock()
 	defer t.mu.Unlock()
@@ -167,18 +174,26 @@ func (t *Tracker) Satisfied(ctx context.Context) bool {
 // Run runs the tracker and blocks until it completes.
 // The provided context can be cancelled to signal a shutdown request.
 func (t *Tracker) Run(ctx context.Context) error {
-	var grp errgroup.Group
-	t.constraintTrackers = syncutil.RunnerWithContext(ctx)
+	// Any failure in the errgroup will cancel goroutines in the group using gctx.
+	// The odd one out is the statsPrinter which is meant to outlive the tracking
+	// routines.
+	grp, gctx := errgroup.WithContext(ctx)
+	t.constraintTrackers = syncutil.RunnerWithContext(gctx)
 	close(t.ready) // The constraintTrackers SingleRunner is ready.
 
 	grp.Go(func() error {
-		return t.trackConstraintTemplates(ctx)
+		return t.trackConstraintTemplates(gctx)
 	})
 	grp.Go(func() error {
-		return t.trackConfig(ctx)
+		return t.trackConfig(gctx)
+	})
+	grp.Go(func() error {
+		t.statsPrinter(ctx)
+		return nil
 	})
 
 	_ = grp.Wait()
+	_ = t.constraintTrackers.Wait() // Must appear after grp.Wait() - allows trackConstraintTemplates() time to schedule its sub-tasks.
 	return nil
 }
 
@@ -354,6 +369,83 @@ func (t *Tracker) trackConstraints(ctx context.Context, gvk schema.GroupVersionK
 	}
 
 	return nil
+}
+
+// EnableStats enables the verbose logging routine for the readiness tracker.
+func (t *Tracker) EnableStats(ctx context.Context) {
+	t.statsEnabled.Set(true)
+}
+
+// DisableStats disables the verbose logging routine for the readiness tracker.
+func (t *Tracker) DisableStats(ctx context.Context) {
+	t.statsEnabled.Set(false)
+}
+
+// statsPrinter handles verbose logging of the readiness tracker outstanding expectations on a regular cadence.
+// Runs until the provided context is cancelled.
+func (t *Tracker) statsPrinter(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(statsPeriod):
+		}
+
+		if !t.statsEnabled.Get() {
+			continue
+		}
+
+		if t.Satisfied(ctx) {
+			return
+		}
+
+		if unsat := t.templates.unsatisfied(); len(unsat) > 0 {
+			log.Info("--- begin unsatisfied templates ---", "populated", t.templates.Populated(), "count", len(unsat))
+			for _, u := range unsat {
+				log.Info("unsatisfied template", "name", u.namespacedName, "gvk", u.gvk)
+			}
+		}
+
+		for _, gvk := range t.templates.kinds() {
+			if t.constraints.Get(gvk).Satisfied() {
+				continue
+			}
+			c := t.constraints.Get(gvk)
+			tr, ok := c.(*objectTracker)
+			if !ok {
+				continue
+			}
+			unsat := tr.unsatisfied()
+			if len(unsat) == 0 {
+				continue
+			}
+
+			log.Info("--- begin unsatisfied constraints ---", "gvk", gvk, "populated", tr.Populated(), "count", len(unsat))
+			for _, u := range unsat {
+				log.Info("unsatisfied constraint", "name", u.namespacedName, "gvk", u.gvk)
+			}
+		}
+
+		for _, gvk := range t.config.kinds() {
+			if t.data.Get(gvk).Satisfied() {
+				continue
+			}
+			c := t.data.Get(gvk)
+			tr, ok := c.(*objectTracker)
+			if !ok {
+				continue
+			}
+			unsat := tr.unsatisfied()
+			if len(unsat) == 0 {
+				continue
+			}
+
+			log.Info("--- Begin unsatisfied data ---", "gvk", gvk, "populated", tr.Populated(), "count", len(unsat))
+			for _, u := range unsat {
+				log.Info("unsatisfied data", "name", u.namespacedName, "gvk", u.gvk)
+			}
+		}
+	}
 }
 
 // Returns the constraint GVK that would be generated by a template.

pkg/syncutil/syncbool.go
@@ -13,23 +13,27 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package readiness
+package syncutil
 
-import (
-	"fmt"
+import "sync/atomic"
 
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
-)
-
-type objKey struct {
-	gvk            schema.GroupVersionKind
-	namespacedName types.NamespacedName
+// SyncBool represents a synchronized boolean flag.
+// Its methods are safe to call concurrently.
+type SyncBool struct {
+	val int32
 }
 
-type objSet map[objKey]struct{}
+// Set sets the values of the flag.
+func (b *SyncBool) Set(v bool) {
+	if v {
+		atomic.StoreInt32(&b.val, 1)
+	} else {
+		atomic.StoreInt32(&b.val, 0)
+	}
+}
 
-func (k objKey) String() string {
-	return fmt.Sprintf("%s [%s]", k.namespacedName.String(), k.gvk.String())
-
+// Get returns the current value of the flag.
+func (b *SyncBool) Get() bool {
+	v := atomic.LoadInt32(&b.val)
+	return v == 1
 }

pkg/controller/config/config_controller_test.go
@@ -284,7 +284,7 @@ func TestConfig_DeleteSyncResources(t *testing.T) {
 
 	// ensure that expectations are set for the constraint gvk
 	g.Eventually(func() bool {
-		return tr.ExpectedContains(gvk, types.NamespacedName{Name: "testpod", Namespace: "default"})
+		return tr.IsExpecting(gvk, types.NamespacedName{Name: "testpod", Namespace: "default"})
 	}, timeout).Should(gomega.BeTrue())
 
 	// delete the pod , the delete event will be reconciled by sync controller
@@ -602,5 +602,5 @@ func unstructuredFor(gvk schema.GroupVersionKind, name string) *unstructured.Uns
 
 // This interface is getting used by tests to check the private objects of objectTracker
 type testExpectations interface {
-	ExpectedContains(gvk schema.GroupVersionKind, nsName types.NamespacedName) bool
+	IsExpecting(gvk schema.GroupVersionKind, nsName types.NamespacedName) bool
 }

pkg/controller/constrainttemplate/constrainttemplate_controller_test.go
@@ -383,7 +383,7 @@ anyrule[}}}//invalid//rego
 	})
 }
 
-// tests that expectations forconstraint gets cancelled when it gets deleted
+// Tests that expectations for constraints are cancelled if the corresponding constraint is deleted.
 func TestReconcile_DeleteConstraintResources(t *testing.T) {
 	log.Info("Running test: Cancel the expectations when constraint gets deleted")
 
@@ -459,7 +459,7 @@ violation[{"msg": "denied!"}] {
 		t.Fatalf("unable to set up OPA client: %s", err)
 	}
 
-	os.Setenv("POD_NAME", "no-pod")
+	_ = os.Setenv("POD_NAME", "no-pod")
 	podstatus.DisablePodOwnership()
 	cs := watch.NewSwitch()
 	pod := &corev1.Pod{}
@@ -472,15 +472,25 @@ violation[{"msg": "denied!"}] {
 	// start manager that will start tracker and controller
 	stopMgr, mgrStopped := StartTestManager(mgr, g)
 
+	once := sync.Once{}
+	testMgrStopped := func() {
+		once.Do(func() {
+			close(stopMgr)
+			mgrStopped.Wait()
+		})
+	}
+	defer testMgrStopped()
+
 	// get the object tracker for the constraint
-	tr, ok := tracker.For(gvk).(testExpectations)
+	ot := tracker.For(gvk)
+	tr, ok := ot.(testExpectations)
 	if !ok {
-		t.Fatalf("unexpected tracker, got %T", tr)
+		t.Fatalf("unexpected tracker, got %T", ot)
 	}
 	// ensure that expectations are set for the constraint gvk
 	g.Eventually(func() bool {
-		return tr.ExpectedContains(gvk, types.NamespacedName{Name: "denyallconstraint"})
-	}, timeout).Should(gomega.BeTrue())
+		return tr.IsExpecting(gvk, types.NamespacedName{Name: "denyallconstraint"})
+	}, timeout).Should(gomega.BeTrue(), "waiting for expectations")
 
 	// Delete the constraint , the delete event will be reconciled by controller
 	// to cancel the expectation set for it by tracker
@@ -496,16 +506,6 @@ violation[{"msg": "denied!"}] {
 	g.Eventually(func() bool {
 		return tracker.For(gvk).Satisfied()
 	}, timeout).Should(gomega.BeTrue())
-
-	once := sync.Once{}
-	testMgrStopped := func() {
-		once.Do(func() {
-			close(stopMgr)
-			mgrStopped.Wait()
-		})
-	}
-	defer testMgrStopped()
-
 }
 
 func constraintEnforced(c client.Client, g *gomega.GomegaWithT, timeout time.Duration) {
@@ -656,5 +656,5 @@ func ignoreNotFound(err error) error {
 
 // This interface is getting used by tests to check the private objects of objectTracker
 type testExpectations interface {
-	ExpectedContains(gvk schema.GroupVersionKind, nsName types.NamespacedName) bool
+	IsExpecting(gvk schema.GroupVersionKind, nsName types.NamespacedName) bool
 }

pkg/readiness/object_tracker_test.go
@@ -208,11 +208,38 @@ func Test_ObjectTracker_CircuitBreaker(t *testing.T) {
 
 	g.Expect(ot.Satisfied()).To(gomega.BeTrue())
 
-	// Peek at internals - we should no be accruing memory from the post-circuit-breaker operations
+	// Peek at internals - we should not be accruing memory from the post-circuit-breaker operations
 	ot.mu.RLock()
 	defer ot.mu.RUnlock()
 	g.Expect(ot.cancelled).To(gomega.BeEmpty())
 	g.Expect(ot.expect).To(gomega.BeEmpty())
 	g.Expect(ot.seen).To(gomega.BeEmpty())
 	g.Expect(ot.satisfied).To(gomega.BeEmpty())
+}
+
+// Verifies the kinds internal method and that it retains it values even after
+// the circuit-breaker trips (which releases memory it depends on).
+func Test_ObjectTracker_kinds(t *testing.T) {
+	g := gomega.NewWithT(t)
+	ot := newObjTracker(schema.GroupVersionKind{})
+
+	const count = 10
+	ct := makeCTSlice("ct-", count)
+	for i := 0; i < len(ct); i++ {
+		ot.Expect(ct[i])
+	}
+	ot.ExpectationsDone()
+
+	kindsBefore := ot.kinds()
+
+	for i := 0; i < len(ct); i++ {
+		ot.Observe(ct[i])
+	}
+
+	g.Expect(ot.Satisfied()).Should(gomega.BeTrue(), "should be satisfied")
+
+	kindsAfter := ot.kinds()
+
+	g.Expect(kindsBefore).ShouldNot(gomega.BeEmpty(), "expected non-empty kinds")
+	g.Expect(kindsAfter).Should(gomega.Equal(kindsBefore), "expected kinds to match")
 }

pkg/syncutil/syncbool_test.go
@@ -13,23 +13,35 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package readiness
+package syncutil_test
 
 import (
-	"fmt"
+	"testing"
+	"time"
 
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
+	"github.com/open-policy-agent/gatekeeper/pkg/syncutil"
 )
 
-type objKey struct {
-	gvk            schema.GroupVersionKind
-	namespacedName types.NamespacedName
-}
+// Verifies changes are visible across goroutines.
+func Test_SyncBool(t *testing.T) {
+	var b syncutil.SyncBool
+	done := make(chan struct{})
 
-type objSet map[objKey]struct{}
+	go func() {
+		defer close(done)
 
-func (k objKey) String() string {
-	return fmt.Sprintf("%s [%s]", k.namespacedName.String(), k.gvk.String())
+		for b.Get() == false {
+		}
+	}()
 
+	go func() {
+		b.Set(true)
+	}()
+
+	select {
+	case <-time.After(10 * time.Millisecond):
+		t.Errorf("failed waiting for flag visibility across goroutines")
+	case <-done:
+		// Success
+	}
 }