9f8937171f5f59c06518197f0d737d1fd91c8ea0
Matej Kern <matej.kern@gmail.com>
2020-10-09 09:20:33 +0200

    Fix lint errors
    Signed-off-by: Matej Kern <matej.kern@gmail.com>
    Signed-off-by: Matej Kern <matej.kern@infobip.com>

--- cmd/build/helmify/replacements.go
+++ cmd/build/helmify/replacements.go
@@ -4,7 +4,7 @@ var replacements = map[string]string{
 	"HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_CONTAINER_RESOURCES": `
 {{ toYaml .Values.controllerManager.resources | indent 10 }}`,
 
-"HELMSUBST_DEPLOYMENT_AUDIT_CONTAINER_RESOURCES": `
+	"HELMSUBST_DEPLOYMENT_AUDIT_CONTAINER_RESOURCES": `
 {{ toYaml .Values.audit.resources | indent 10 }}`,
 
 	"HELMSUBST_DEPLOYMENT_POD_SCHEDULING": `
