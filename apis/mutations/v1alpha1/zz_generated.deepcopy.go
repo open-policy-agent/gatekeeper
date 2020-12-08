6bad8bcb5bcea4e4be5c946114a939132192be66
juliankatz <juliankatz@google.com>
2020-12-08 14:49:14 -0800

    Add the readiness-retries flag, allowing the user to configure how many attempts should be made to injest a resource into OPA while blocking the webhook.
    Previously, a failure to injest an object into the OPA cache would
    strike that object from the ObjectTracker, a type tasked with blocking
    the webhook from serving requests until all the necessary objects had
    been observed on the API server.  That setup optimizes for availability
    (the webhook serving requests ASAP) over security.
    
    By adding retry functionality, we configure gatekeeper to block the
    webhook for longer, but unblock with a more complete set of constraints.
    
    fixes: #213
    
    Signed-off-by: juliankatz <juliankatz@google.com>

--- apis/mutations/v1alpha1/zz_generated.deepcopy.go
+++ apis/mutations/v1alpha1/zz_generated.deepcopy.go
@@ -20,7 +20,7 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
