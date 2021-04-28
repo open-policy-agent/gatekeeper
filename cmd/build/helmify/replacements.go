a974d70df29a10298cd05a74b412297de5402823
Chris Stanaway <jcstanaway@gmail.com>
2021-04-28 02:33:39 -0500

    Add priority class name (#1266)
    * Add support for priorityClassName (#1252)
    
    Signed-off-by: Chris Stanaway <chris.stanaway@here.com>
    
    * Add support for priorityClassName (#1252)
    
    Signed-off-by: Chris Stanaway <chris.stanaway@here.com>
    
    * Removed changes to `charts` directory per instructions. Updated helm chart README to reference `kustomize-for-helm.yaml`.
    
    Signed-off-by: Chris Stanaway <chris.stanaway@here.com>
    
    * Added priorityClassName to the default deployment
    
    Signed-off-by: Chris Stanaway <chris.stanaway@here.com>
    
    * Moved README update
    
    Signed-off-by: Chris Stanaway <chris.stanaway@here.com>
    
    * Add newline at end of file
    
    Signed-off-by: Chris Stanaway <chris.stanaway@here.com>
    
    * Moved README update
    
    Signed-off-by: Chris Stanaway <chris.stanaway@here.com>
    
    Co-authored-by: Rita Zhang <rita.z.zhang@gmail.com>

--- cmd/build/helmify/replacements.go
+++ cmd/build/helmify/replacements.go
@@ -17,6 +17,8 @@ var replacements = map[string]string{
 
 	`HELMSUBST_DEPLOYMENT_AUDIT_IMAGE_PULL_SECRETS: ""`: `{{- toYaml .Values.image.pullSecrets | nindent 8 }}`,
 
+	"HELMSUBST_DEPLOYMENT_AUDIT_PRIORITY_CLASS_NAME": `{{ .Values.audit.priorityClassName }}`,
+
 	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_NODE_SELECTOR: ""`: `{{- toYaml .Values.controllerManager.nodeSelector | nindent 8 }}`,
 
 	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_AFFINITY: ""`: `{{- toYaml .Values.controllerManager.affinity | nindent 8 }}`,
@@ -24,6 +26,8 @@ var replacements = map[string]string{
 	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_TOLERATIONS: ""`: `{{- toYaml .Values.controllerManager.tolerations | nindent 8 }}`,
 
 	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_IMAGE_PULL_SECRETS: ""`: `{{- toYaml .Values.image.pullSecrets | nindent 8 }}`,
+
+	"HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_PRIORITY_CLASS_NAME": `{{ .Values.controllerManager.priorityClassName }}`,
 
 	"HELMSUBST_DEPLOYMENT_REPLICAS": `{{ .Values.replicas }}`,
 
