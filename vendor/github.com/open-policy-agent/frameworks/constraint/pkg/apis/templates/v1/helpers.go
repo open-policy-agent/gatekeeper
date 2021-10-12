313e832d871a5a871a9719a2d5f308c95c7f4fd0
Sertac Ozercan <sozercan@gmail.com>
2021-10-12 23:38:33 +0000

    vendor frameworks a8579b618cb7
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- vendor/github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1/helpers.go
+++ vendor/github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1/helpers.go
@@ -8,10 +8,10 @@ import (
 // versionless api representation.
 func (versioned *ConstraintTemplate) ToVersionless() (*templates.ConstraintTemplate, error) {
 	versionedCopy := versioned.DeepCopy()
-	Scheme.Default(versionedCopy)
+	versionedScheme.Default(versionedCopy)
 
 	versionless := &templates.ConstraintTemplate{}
-	if err := Scheme.Convert(versionedCopy, versionless, nil); err != nil {
+	if err := versionedScheme.Convert(versionedCopy, versionless, nil); err != nil {
 		return nil, err
 	}
 
