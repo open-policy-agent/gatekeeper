e55ea5178507fd6122c59117ec0351720b28e0e5
Max Smythe <smythe@google.com>
2022-09-16 14:43:32 -0700

    perf: Upgrade Constraint Framework to v0.7.0
    This upgrades opa to v0.44.0 and should reduce
    lock contention between data caching and policy evaluation.
    
    Signed-off-by: Max Smythe <smythe@google.com>

--- vendor/github.com/open-policy-agent/opa/bundle/store.go
+++ vendor/github.com/open-policy-agent/opa/bundle/store.go
@@ -390,10 +390,6 @@ func activateBundles(opts *ActivateOpts) error {
 
 		if b.lazyLoadingMode {
 
-			if len(b.Raw) == 0 {
-				return fmt.Errorf("raw bundle bytes not set on bundle object")
-			}
-
 			for _, item := range b.Raw {
 				path := filepath.ToSlash(item.Path)
 
@@ -727,6 +723,8 @@ func writeDataAndModules(ctx context.Context, store storage.Store, txn storage.T
 				}
 			}
 		} else {
+			params.BasePaths = *b.Manifest.Roots
+
 			err := store.Truncate(ctx, txn, params, NewIterator(b.Raw))
 			if err != nil {
 				return fmt.Errorf("store truncate failed for bundle '%s': %v", name, err)
