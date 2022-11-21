21345e1bfe31a57214e05689366199c67ef96897
Jaydipkumar Arvindbhai Gabani <gabanijaydip@gmail.com>
2022-11-21 10:14:25 -0800

    chore: Verify CN name as part of client cert check while authenticating api server (#2396)
    Fixes https://github.com/open-policy-agent/gatekeeper/issues/2390

--- pkg/webhook/common.go
+++ pkg/webhook/common.go
@@ -2,6 +2,7 @@ package webhook
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"flag"
 	"fmt"
@@ -20,7 +21,6 @@ import (
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
@@ -61,7 +61,8 @@ var (
 	emitAdmissionEvents                = flag.Bool("emit-admission-events", false, "(alpha) emit Kubernetes events in gatekeeper namespace for each admission violation")
 	tlsMinVersion                      = flag.String("tls-min-version", "1.3", "minimum version of TLS supported")
 	serviceaccount                     = fmt.Sprintf("system:serviceaccount:%s:%s", util.GetNamespace(), serviceAccountName)
-	clientCAName                       = flag.String("client-cert-name", "", "name of the client certificate to authenticate apiserver request against")
+	clientCAName                       = flag.String("client-ca-name", "", "name of the certificate authority bundle to authenticate the Kubernetes API server requests against")
+	certCNName                         = flag.String("client-cn-name", "kube-apiserver", "expected CN name on the client certificate attached by apiserver in requests to the webhook")
 	// webhookName is deprecated, set this on the manifest YAML if needed".
 )
 
@@ -148,11 +149,27 @@ func (h *webhookHandler) skipExcludedNamespace(req *admissionv1.AdmissionRequest
 	return isNamespaceExcluded, err
 }
 
-func getServerConfig(mgr manager.Manager) *webhook.Server {
-	server := mgr.GetWebhookServer()
+func congifureWebhookServer(server *webhook.Server) *webhook.Server {
 	server.TLSMinVersion = *tlsMinVersion
 	if *clientCAName != "" {
 		server.ClientCAName = *clientCAName
+		server.TLSOpts = []func(*tls.Config){
+			func(cfg *tls.Config) {
+				cfg.VerifyConnection = getCertNameVerifier()
+			},
+		}
 	}
 	return server
+}
+
+func getCertNameVerifier() func(cs tls.ConnectionState) error {
+	return func(cs tls.ConnectionState) error {
+		if len(cs.PeerCertificates) > 0 {
+			if cs.PeerCertificates[0].Subject.CommonName != *certCNName {
+				return fmt.Errorf("x509: subject with cn=%s do not identify as %s", cs.PeerCertificates[0].Subject.CommonName, *certCNName)
+			}
+			return nil
+		}
+		return fmt.Errorf("Failed to verify CN name of certificate")
+	}
 }
---
+++ pkg/webhook/common_test.go
@@ -0,0 +1,344 @@
+package webhook
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"io"
+	lg "log"
+	"math/big"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+type chanWriter chan string
+
+func (w chanWriter) Write(p []byte) (n int, err error) {
+	w <- string(p)
+	return len(p), nil
+}
+
+func TestCongifureWebhookServer(t *testing.T) {
+	expectedServer := &webhook.Server{
+		TLSMinVersion: "1.3",
+	}
+
+	if *clientCAName != "" {
+		expectedServer.ClientCAName = *clientCAName
+	}
+
+	tc := []struct {
+		Name           string
+		Server         *webhook.Server
+		ExpectedServer *webhook.Server
+	}{
+		{
+			Name:           "Wbhook server config",
+			Server:         &webhook.Server{},
+			ExpectedServer: expectedServer,
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.Name, func(t *testing.T) {
+			server := congifureWebhookServer(tt.Server)
+			expectedServer.TLSOpts = server.TLSOpts
+
+			if !reflect.DeepEqual(tt.ExpectedServer, server) {
+				t.Errorf(fmt.Sprintf("got %#v, want %#v", server, tt.ExpectedServer))
+			}
+		})
+	}
+}
+
+func TestTLSConfig(t *testing.T) {
+	ca, caPEM, caPrivKey, err := getCA(*certCNName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	certpool := x509.NewCertPool()
+	certpool.AppendCertsFromPEM(caPEM.Bytes())
+
+	serverTLSConf, err := serverCertSetup(*certCNName, ca, caPrivKey, certpool)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "success!")
+	}))
+	ts.TLS = serverTLSConf
+	errc := make(chanWriter, 10)
+	ts.Config.ErrorLog = lg.New(errc, "", 0)
+	ts.StartTLS()
+	defer ts.Close()
+
+	goodHTTPClient, err := getClient(*certCNName, ca, caPrivKey, certpool)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	badHTTPClient, err := getClient("test", ca, caPrivKey, certpool)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	diffCa, diffCaPEM, diffCaPrivKey, err := getCA(*certCNName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	diffCertpool := x509.NewCertPool()
+	diffCertpool.AppendCertsFromPEM(diffCaPEM.Bytes())
+
+	diffHTTPClient, err := getClient(*certCNName, diffCa, diffCaPrivKey, diffCertpool)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tc := []struct {
+		Name      string
+		WantError bool
+		Msg       string
+		Client    *http.Client
+		Error     error
+	}{
+		{
+			Name:      "Connecting to server with valid certificate that has expected CN name",
+			WantError: false,
+			Msg:       "success!",
+			Client:    goodHTTPClient,
+			Error:     nil,
+		},
+		{
+			Name:      "Connecting to server with valid certificate but with wrong CN name",
+			WantError: true,
+			Msg:       "",
+			Client:    badHTTPClient,
+			Error:     fmt.Errorf("Get \"%s\": remote error: tls: bad certificate", ts.URL),
+		},
+		{
+			Name:      "Connecting to server with invalid certificate that has expected CN name",
+			WantError: true,
+			Msg:       "",
+			Client:    diffHTTPClient,
+			Error:     fmt.Errorf("x509: certificate signed by unknown authority"),
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.Name, func(t *testing.T) {
+			resp, err := tt.Client.Get(ts.URL)
+			assert.Equal(t, err != nil, tt.WantError)
+			if tt.WantError {
+				assert.ErrorContains(t, err, tt.Error.Error())
+			} else {
+				// verify the response
+				respBodyBytes, err := io.ReadAll(resp.Body)
+				if err != nil {
+					t.Fatal(err)
+				}
+				body := strings.TrimSpace(string(respBodyBytes))
+				assert.Equal(t, body, tt.Msg)
+			}
+		})
+	}
+
+	select {
+	case v := <-errc:
+		if !strings.Contains(v, fmt.Sprintf("x509: subject with cn=test do not identify as %s", *certCNName)) {
+			t.Errorf("expected an error log message containing '%s'; got %q", fmt.Sprintf("x509: subject with cn=test do not identify as %s", *certCNName), v)
+		}
+	case <-time.After(5 * time.Second):
+		t.Errorf("timeout waiting for logged error")
+	}
+}
+
+// returns a CA, CA PEM and private key.
+func getCA(s string) (ca *x509.Certificate, caPEM *bytes.Buffer, caPrivKey *rsa.PrivateKey, err error) {
+	// set up our CA certificate
+	ca = &x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject: pkix.Name{
+			CommonName: s,
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	// create private and public key
+	caPrivKey, err = rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// create the CA
+	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	// pem encode
+	caPEM = new(bytes.Buffer)
+	err = pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	caPrivKeyPEM := new(bytes.Buffer)
+	err = pem.Encode(caPrivKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey),
+	})
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return
+}
+
+// return server TLS configurations.
+func serverCertSetup(s string, ca *x509.Certificate, caPrivKey *rsa.PrivateKey, certPool *x509.CertPool) (serverTLSConf *tls.Config, err error) {
+	// set up server certificate
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject: pkix.Name{
+			CommonName: s,
+		},
+		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}
+
+	certPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &certPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return nil, err
+	}
+
+	certPEM := new(bytes.Buffer)
+	err = pem.Encode(certPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	certPrivKeyPEM := new(bytes.Buffer)
+	err = pem.Encode(certPrivKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(certPrivKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	serverCert, err := tls.X509KeyPair(certPEM.Bytes(), certPrivKeyPEM.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	serverTLSConf = &tls.Config{
+		Certificates:     []tls.Certificate{serverCert},
+		VerifyConnection: getCertNameVerifier(),
+		ClientCAs:        certPool,
+		ClientAuth:       tls.RequireAndVerifyClientCert,
+		MinVersion:       tls.VersionTLS13,
+	}
+	return
+}
+
+// returns http client that can connect to TLS server.
+func getClient(s string, ca *x509.Certificate, caPrivKey *rsa.PrivateKey, certPool *x509.CertPool) (httpClient *http.Client, err error) {
+	// set up client certificate
+	client := &x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject: pkix.Name{
+			CommonName: s,
+		},
+		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}
+
+	clientCertPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+
+	clientCertBytes, err := x509.CreateCertificate(rand.Reader, client, ca, &clientCertPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return nil, err
+	}
+
+	clientCertPEM := new(bytes.Buffer)
+	err = pem.Encode(clientCertPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: clientCertBytes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	clientCertPrivKeyPEM := new(bytes.Buffer)
+	err = pem.Encode(clientCertPrivKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(clientCertPrivKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	clientCert, err := tls.X509KeyPair(clientCertPEM.Bytes(), clientCertPrivKeyPEM.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	clientTLSConf := &tls.Config{
+		RootCAs:      certPool,
+		Certificates: []tls.Certificate{clientCert},
+		MinVersion:   tls.VersionTLS13,
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: clientTLSConf,
+	}
+
+	httpClient = &http.Client{
+		Transport: transport,
+	}
+
+	return
+}
