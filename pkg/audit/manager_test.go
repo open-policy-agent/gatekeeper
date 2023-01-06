68f822860b436e0c06625e584a4b2912b48c53ec
Sertac Ozercan <sozercan@gmail.com>
2023-01-06 22:07:33 +0000

    use t.run
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/audit/manager_test.go
+++ pkg/audit/manager_test.go
@@ -33,66 +33,85 @@ func Test_newNSCache(t *testing.T) {
 }
 
 func Test_truncateString(t *testing.T) {
-	// Test case 1
-	str := "Hello world!"
-	size := len(str)
-	expected := str
-	result := truncateString(str, size)
-	if result != expected {
-		t.Errorf("Unexpected result for str = %q, size = %d: got %q, want %q", str, size, result, expected)
+	type args struct {
+		str  string
+		size int
 	}
-
-	// Test case 2
-	str = "Hello world!"
-	size = 5
-	expected = "He..."
-	result = truncateString(str, size)
-	if result != expected {
-		t.Errorf("Unexpected result for str = %q, size = %d: got %q, want %q", str, size, result, expected)
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "test 1",
+			args: args{
+				str:  "Hello world!",
+				size: len("Hello world!"),
+			},
+			want: "Hello world!",
+		},
+		{
+			name: "test 2",
+			args: args{
+				str:  "Hello world!",
+				size: 5,
+			},
+			want: "He...",
+		},
+		{
+			name: "test 3",
+			args: args{
+				str:  "Hello, world!",
+				size: 0,
+			},
+			want: "...",
+		},
 	}
-
-	// Test case 3
-	str = "Hello, world!"
-	size = 0
-	expected = "..."
-	result = truncateString(str, size)
-	if result != expected {
-		t.Errorf("Unexpected result for str = %q, size = %d: got %q, want %q", str, size, result, expected)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := truncateString(tt.args.str, tt.args.size); got != tt.want {
+				t.Errorf("truncateString() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func Test_mergeErrors(t *testing.T) {
-	// one error
-	errs := []error{errors.New("error 1")}
-	expected := "error 1"
-	result := mergeErrors(errs)
-	if result == nil || result.Error() != expected {
-		t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
-	}
+	t.Run("one error", func(t *testing.T) {
+		errs := []error{errors.New("error 1")}
+		expected := "error 1"
+		result := mergeErrors(errs)
+		if result == nil || result.Error() != expected {
+			t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
+		}
+	})
 
-	// empty errors
-	errs = []error{}
-	expected = ""
-	result = mergeErrors(errs)
-	if result.Error() != expected {
-		t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
-	}
+	t.Run("empty errors", func(t *testing.T) {
+		errs := []error{}
+		expected := ""
+		result := mergeErrors(errs)
+		if result.Error() != expected {
+			t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
+		}
+	})
 
-	// 3 errors
-	errs = []error{errors.New("error 1"), errors.New("error 2"), errors.New("error 3")}
-	expected = "error 1\nerror 2\nerror 3"
-	result = mergeErrors(errs)
-	if result == nil || result.Error() != expected {
-		t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
-	}
+	t.Run("3 errors", func(t *testing.T) {
+		errs := []error{errors.New("error 1"), errors.New("error 2"), errors.New("error 3")}
+		expected := "error 1\nerror 2\nerror 3"
+		result := mergeErrors(errs)
+		if result == nil || result.Error() != expected {
+			t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
+		}
+	})
 
-	// 2 errors with newlines
-	errs = []error{errors.New("error 1\nerror 1.1"), errors.New("error 2\nerror 2.2")}
-	expected = "error 1\nerror 1.1\nerror 2\nerror 2.2"
-	result = mergeErrors(errs)
-	if result == nil || result.Error() != expected {
-		t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
-	}
+	t.Run("2 errors with newlines", func(t *testing.T) {
+		errs := []error{errors.New("error 1\nerror 1.1"), errors.New("error 2\nerror 2.2")}
+		expected := "error 1\nerror 1.1\nerror 2\nerror 2.2"
+		result := mergeErrors(errs)
+		if result == nil || result.Error() != expected {
+			t.Errorf("Unexpected result for errs = %v: got %v, want %v", errs, result, expected)
+		}
+	})
 }
 
 func Test_nsMapFromObjs(t *testing.T) {
@@ -224,110 +243,121 @@ func Test_getViolationRef(t *testing.T) {
 }
 
 func Test_getFilesFromDir(t *testing.T) {
-	// Test case 1: directory does not exist
 	am := Manager{}
-	_, err := am.getFilesFromDir("/does/not/exist", 10)
-	if err == nil {
-		t.Errorf("Expected error when directory does not exist, got nil")
-	}
 
-	// Test case 2: directory exists and is empty
-	emptyDir, err := os.MkdirTemp("", "empty-dir")
-	if err != nil {
-		t.Errorf("Failed to create temporary directory: %v", err)
-	}
-	defer os.RemoveAll(emptyDir)
-	files, err := am.getFilesFromDir(emptyDir, 10)
-	if err != nil {
-		t.Errorf("Unexpected error when directory is empty: %v", err)
-	}
-	if len(files) != 0 {
-		t.Errorf("Expected 0 files when directory is empty, got %d", len(files))
-	}
-
-	// Test case 3: directory exists and has some files
-	tempDir, err := os.MkdirTemp("", "temp-dir")
-	if err != nil {
-		t.Errorf("Failed to create temporary directory: %v", err)
-	}
-	defer os.RemoveAll(tempDir)
-	for i := 0; i < 15; i++ {
-		file, err := os.CreateTemp(tempDir, "test-file-*.txt")
-		if err != nil {
-			t.Errorf("Failed to create temporary file: %v", err)
+	t.Run("Test case 1: directory does not exist", func(t *testing.T) {
+		_, err := am.getFilesFromDir("/does/not/exist", 10)
+		if err == nil {
+			t.Errorf("Expected error when directory does not exist, got nil")
 		}
-		file.Close()
-	}
-	files, err = am.getFilesFromDir(tempDir, 10)
-	if err != nil {
-		t.Errorf("Unexpected error when directory has files: %v", err)
-	}
-	if len(files) != 15 {
-		t.Errorf("Expected 15 files when directory has 15 files, got %d", len(files))
-	}
+	})
+
+	t.Run("Test case 2: directory exists and is empty", func(t *testing.T) {
+		emptyDir, err := os.MkdirTemp("", "empty-dir")
+		if err != nil {
+			t.Errorf("Failed to create temporary directory: %v", err)
+		}
+		defer os.RemoveAll(emptyDir)
+		files, err := am.getFilesFromDir(emptyDir, 10)
+		if err != nil {
+			t.Errorf("Unexpected error when directory is empty: %v", err)
+		}
+		if len(files) != 0 {
+			t.Errorf("Expected 0 files when directory is empty, got %d", len(files))
+		}
+	})
+
+	t.Run("Test case 3: directory exists and has some files", func(t *testing.T) {
+		tempDir, err := os.MkdirTemp("", "temp-dir")
+		if err != nil {
+			t.Errorf("Failed to create temporary directory: %v", err)
+		}
+		defer os.RemoveAll(tempDir)
+		for i := 0; i < 15; i++ {
+			file, err := os.CreateTemp(tempDir, "test-file-*.txt")
+			if err != nil {
+				t.Errorf("Failed to create temporary file: %v", err)
+			}
+			file.Close()
+		}
+		files, err := am.getFilesFromDir(tempDir, 10)
+		if err != nil {
+			t.Errorf("Unexpected error when directory has files: %v", err)
+		}
+		if len(files) != 15 {
+			t.Errorf("Expected 15 files when directory has 15 files, got %d", len(files))
+		}
+	})
 }
 
 func Test_removeAllFromDir(t *testing.T) {
-	// Test case 1: directory does not exist
 	am := Manager{}
-	err := am.removeAllFromDir("/does/not/exist", 10)
-	if err == nil {
-		t.Errorf("Expected error when directory does not exist, got nil")
-	}
 
-	// Test case 2: directory exists and is empty
-	emptyDir, err := os.MkdirTemp("", "empty-dir")
-	if err != nil {
-		t.Errorf("Failed to create temporary directory: %v", err)
-	}
-	defer os.RemoveAll(emptyDir)
-	err = am.removeAllFromDir(emptyDir, 10)
-	if err != nil {
-		t.Errorf("Unexpected error when directory is empty: %v", err)
-	}
-
-	// Test case 3: directory exists and has some files
-	tempDir, err := os.MkdirTemp("", "temp-dir")
-	if err != nil {
-		t.Errorf("Failed to create temporary directory: %v", err)
-	}
-	defer os.RemoveAll(tempDir)
-	for i := 0; i < 15; i++ {
-		file, err := os.CreateTemp(tempDir, "test-file-*.txt")
-		if err != nil {
-			t.Errorf("Failed to create temporary file: %v", err)
+	t.Run("Test case 1: directory does not exist", func(t *testing.T) {
+		err := am.removeAllFromDir("/does/not/exist", 10)
+		if err == nil {
+			t.Errorf("Expected error when directory does not exist, got nil")
 		}
-		file.Close()
-	}
-	err = am.removeAllFromDir(tempDir, 10)
-	if err != nil {
-		t.Errorf("Unexpected error when removing files from directory: %v", err)
-	}
-	files, err := am.getFilesFromDir(tempDir, 10)
-	if err != nil {
-		t.Errorf("Unexpected error when checking if directory is empty: %v", err)
-	}
-	if len(files) != 0 {
-		t.Errorf("Expected 0 files when all files have been removed, got %d", len(files))
-	}
+	})
+
+	t.Run("Test case 2: directory exists and is empty", func(t *testing.T) {
+		emptyDir, err := os.MkdirTemp("", "empty-dir")
+		if err != nil {
+			t.Errorf("Failed to create temporary directory: %v", err)
+		}
+		defer os.RemoveAll(emptyDir)
+		err = am.removeAllFromDir(emptyDir, 10)
+		if err != nil {
+			t.Errorf("Unexpected error when directory is empty: %v", err)
+		}
+	})
+
+	t.Run("Test case 3: directory exists and has some files", func(t *testing.T) {
+		tempDir, err := os.MkdirTemp("", "temp-dir")
+		if err != nil {
+			t.Errorf("Failed to create temporary directory: %v", err)
+		}
+		defer os.RemoveAll(tempDir)
+		for i := 0; i < 15; i++ {
+			file, err := os.CreateTemp(tempDir, "test-file-*.txt")
+			if err != nil {
+				t.Errorf("Failed to create temporary file: %v", err)
+			}
+			file.Close()
+		}
+		err = am.removeAllFromDir(tempDir, 10)
+		if err != nil {
+			t.Errorf("Unexpected error when removing files from directory: %v", err)
+		}
+		files, err := am.getFilesFromDir(tempDir, 10)
+		if err != nil {
+			t.Errorf("Unexpected error when checking if directory is empty: %v", err)
+		}
+		if len(files) != 0 {
+			t.Errorf("Expected 0 files when all files have been removed, got %d", len(files))
+		}
+	})
 }
 
 func Test_readUnstructured(t *testing.T) {
-	// Test case 1: invalid JSON
 	am := Manager{}
-	_, err := am.readUnstructured([]byte("invalid json"))
-	if err == nil {
-		t.Errorf("Expected error when input is invalid JSON, got nil")
-	}
 
-	// Test case 2: valid JSON
-	jsonBytes := []byte(`{"apiVersion":"v1","kind":"Namespace","metadata":{"name":"my-namespace"}}
-	`)
-	u, err := am.readUnstructured(jsonBytes)
-	if err != nil {
-		t.Errorf("Unexpected error when input is valid JSON: %v", err)
-	}
-	if u.GetName() != "my-namespace" {
-		t.Errorf("Expected name to be 'my-namespace', got %s", u.GetName())
-	}
+	t.Run("Test case 1: invalid JSON", func(t *testing.T) {
+		_, err := am.readUnstructured([]byte("invalid json"))
+		if err == nil {
+			t.Errorf("Expected error when input is invalid JSON, got nil")
+		}
+	})
+
+	t.Run("Test case 2: valid JSON", func(t *testing.T) {
+		jsonBytes := []byte(`{"apiVersion":"v1","kind":"Namespace","metadata":{"name":"my-namespace"}}
+		`)
+		u, err := am.readUnstructured(jsonBytes)
+		if err != nil {
+			t.Errorf("Unexpected error when input is valid JSON: %v", err)
+		}
+		if u.GetName() != "my-namespace" {
+			t.Errorf("Expected name to be 'my-namespace', got %s", u.GetName())
+		}
+	})
 }
