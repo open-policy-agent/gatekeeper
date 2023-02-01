5cc6b523f280fc7c2592bcc52033df82f949e411
Sertac Ozercan <sozercan@gmail.com>
2023-02-01 18:28:01 +0000

    add vendor manifests back
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

---
+++ build/tooling/vendormanifests.go
@@ -0,0 +1,10 @@
+// This is used for vendoring frameworks to Gatekeeper.
+
+//go:build tools
+// +build tools
+
+package build
+
+import (
+	_ "github.com/open-policy-agent/frameworks/constraint/deploy"
+)
