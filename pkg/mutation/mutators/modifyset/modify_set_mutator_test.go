4ed46632972828380ee827ed73e131389c725a38
alex <8968914+acpana@users.noreply.github.com>
2023-02-21 22:20:29 -0800

    fix: handle empty spec for modifyset (#2585)
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>
    Co-authored-by: Max Smythe <smythe@google.com>

---
+++ pkg/mutation/mutators/modifyset/modify_set_mutator_test.go
@@ -0,0 +1,17 @@
+package modifyset
+
+import (
+	"testing"
+
+	mutationsunversioned "github.com/open-policy-agent/gatekeeper/apis/mutations/unversioned"
+	"github.com/stretchr/testify/require"
+)
+
+// Tests the ModifySet mutator MutatorForModifySet call with an empty spec for graceful handling.
+func Test_ModifySet_emptySpec(t *testing.T) {
+	modifySet := &mutationsunversioned.ModifySet{}
+	mutator, err := MutatorForModifySet(modifySet)
+
+	require.ErrorContains(t, err, "applyTo required for ModifySet mutator")
+	require.Nil(t, mutator)
+}
