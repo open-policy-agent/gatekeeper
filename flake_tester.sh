da34d65306dbf30c001a657f402e41cf5f93a446
davis-haba <davishaba@google.com>
2023-03-30 18:24:03 -0700

    blah \nSigned-off-by: davis-haba <davishaba@google.com>

--- flake_tester.sh
+++ flake_tester.sh
@@ -4,7 +4,7 @@ echo "running pkg/controller tests $n times"
 touch tmp.txt
 for i in $(seq 1 $n)
 do
-  go test ../pkg/controller/... -v -count=1 > tmp.txt
+  go test ./pkg/controller/... -v -count=1 > tmp.txt
   retVal=$?
   if [ $retVal -ne 0 ]; then
     echo "Failure at run ${i}"
