baa2110af3aaff03b5c8d4fe1e9671987cf1dfee
davis-haba <davishaba@google.com>
2023-03-30 17:44:02 -0700

    flake tester \nSigned-off-by: davis-haba <davishaba@google.com>

---
+++ hack/flake_tester.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+n=$1
+echo "running pkg/controller tests $n times"
+touch tmp.txt
+for i in $(seq 1 $n)
+do
+  go test ../pkg/controller/... -v -count=1 > tmp.txt
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    echo "Failure at run ${i}"
+    echo "--- output ---"
+    cat tmp.txt
+    echo "--------------"
+    exit 1
+  else
+    echo "Run ${i} passed"
+  fi
+done
