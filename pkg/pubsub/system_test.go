f4856343de2bc007b4a9ec71445dc2b114bf9487
Jaydipkumar Arvindbhai Gabani <gabanijaydip@gmail.com>
2023-05-25 16:13:48 -0700

    feat: Adding pubsub interface (#2538)
    Co-authored-by: Sertaç Özercan <852750+sozercan@users.noreply.github.com>

---
+++ pkg/pubsub/system_test.go
@@ -0,0 +1,27 @@
+package pubsub
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSystem(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  *System
+	}{
+		{
+			name: "requesting system",
+			want: &System{},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			ret := NewSystem()
+			assert.Equal(t, ret, tc.want)
+		})
+	}
+}
