4858f5d8f6635e16002507d85757e51f07433867
Alex Pana <8968914+acpana@users.noreply.github.com>
2023-06-06 20:07:38 +0000

    lint
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- pkg/controller/sync/sync_controller.go
+++ pkg/controller/sync/sync_controller.go
@@ -121,7 +121,6 @@ func (r *ReconcileSync) Reconcile(ctx context.Context, request reconcile.Request
 		return reconcile.Result{}, nil
 	}
 
-
 	reportMetricsForRenconcileRun := false
 	defer func() {
 		if reportMetricsForRenconcileRun {
