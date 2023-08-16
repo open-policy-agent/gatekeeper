66d84f3ec9879d2942c71e78dd371e290a21ea2d
Max Smythe <smythe@google.com>
2023-08-15 17:46:49 -0700

    feat: Upgrade constraint framework to add new K8s Native Validation driver schema (#2951)
    Signed-off-by: Max Smythe <smythe@google.com>

--- vendor/github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/k8scel/new.go
+++ vendor/github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/k8scel/new.go
@@ -1,14 +1,12 @@
 package k8scel
 
 import (
-	"k8s.io/apiserver/pkg/admission/plugin/cel"
 	"k8s.io/apiserver/pkg/admission/plugin/validatingadmissionpolicy"
 )
 
 func New(args ...Arg) (*Driver, error) {
 	driver := &Driver{
-		validators:     map[string]validatingadmissionpolicy.Validator{},
-		filterCompiler: cel.NewFilterCompiler(),
+		validators: map[string]validatingadmissionpolicy.Validator{},
 	}
 	for _, arg := range args {
 		if err := arg(driver); err != nil {
