7b9dd70ab47a2e6db7b94e0bf73fe3268713ad9f
Sertaç Özercan <852750+sozercan@users.noreply.github.com>
2024-03-20 13:34:27 -0700

    chore: bump frameworks to 359cf1b (#3326)
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- vendor/github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/k8scel/args.go
+++ vendor/github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/k8scel/args.go
@@ -1,5 +1,13 @@
 package k8scel
 
+type vapDefault string
+
+const (
+	VAPGenerationLabel = "gatekeeper.sh/use-vap"
+	VAPDefaultYes      = vapDefault("yes")
+	VAPDefaultNo       = vapDefault("no")
+)
+
 type Arg func(*Driver) error
 
 // GatherStats starts collecting various stats around the
@@ -7,6 +15,21 @@ type Arg func(*Driver) error
 func GatherStats() Arg {
 	return func(driver *Driver) error {
 		driver.gatherStats = true
+
+		return nil
+	}
+}
+
+// VAPGenerationDefault sets the expected default
+// value of the `gatekeeper.sh/use-vap` label.
+// If no value is provided, VAP generation
+// is presumed to be disabled and the engine will
+// validate ALL policies. Otherwise, the engine
+// will only validate policies not expected to be
+// enforced via VAP.
+func VAPGenerationDefault(d vapDefault) Arg {
+	return func(driver *Driver) error {
+		driver.generateVAPDefault = &d
 
 		return nil
 	}
