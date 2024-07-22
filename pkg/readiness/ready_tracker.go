c340a308b94fbc42c1abf6dab3afbdbf42c0ab40
David-Jaeyoon-Lee <davjlee@google.com>
2024-07-22 08:36:19 +0000

    Shorten message for crashOnFailureFetchingExpectations flag
    Signed-off-by: David-Jaeyoon-Lee <davjlee@google.com>

--- pkg/readiness/ready_tracker.go
+++ pkg/readiness/ready_tracker.go
@@ -46,7 +46,7 @@ import (
 
 var log = logf.Log.WithName("readiness-tracker")
 
-var crashOnFailureFetchingExpectations = flag.Bool("crash-on-failure-fetching-expectations", false, "Unless set (defaults to false), gatekeeper will ignore errors that occur when gathering expectations. This prevents bootstrapping errors from crashing Gatekeeper at the cost of increasing the risk Gatekeeper will under-enforce policy by serving before it has loaded in all policies. Enabling this will help prevent under-enforcement at the risk of crashing during startup for issues like network errors. Note that enabling this flag currently does not achieve the aforementioned effect since fetching expectations are set to retry until success so failures during fetching expectations currently do not occur.")
+var crashOnFailureFetchingExpectations = flag.Bool("crash-on-failure-fetching-expectations", false, "Unless set (defaults to false), gatekeeper will ignore errors when gathering expectations. This prevents bootstrapping errors from crashing Gatekeeper at the cost of increasing the risk Gatekeeper will under-enforce policy. Enabling this will help prevent under-enforcement at the risk of crashing during startup. Note that enabling this flag currently does not achieve the aforementioned effect since fetching expectations will retry until success.")
 
 const (
 	constraintGroup = "constraints.gatekeeper.sh"
