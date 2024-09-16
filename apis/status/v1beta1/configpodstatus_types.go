14d80aa64dddaac8dd87addc577c51311d86d944
Avinash Patnala <avinashpatnala@google.com>
2024-09-16 18:44:45 +0000

    generate manifests
    Signed-off-by: Avinash Patnala <avinashpatnala@google.com>

--- apis/status/v1beta1/configpodstatus_types.go
+++ apis/status/v1beta1/configpodstatus_types.go
@@ -6,8 +6,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-
-	// "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
