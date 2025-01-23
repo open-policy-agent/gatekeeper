be29cc3252ce692e91ff8764dca01e845ae847fa
Sertac Ozercan <sozercan@gmail.com>
2025-01-23 19:50:20 +0000

    update opa/v1
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/drivers/k8scel/driver.go
+++ pkg/drivers/k8scel/driver.go
@@ -15,7 +15,7 @@ import (
 	"github.com/open-policy-agent/frameworks/constraint/pkg/types"
 	pSchema "github.com/open-policy-agent/gatekeeper/v3/pkg/drivers/k8scel/schema"
 	"github.com/open-policy-agent/gatekeeper/v3/pkg/drivers/k8scel/transform"
-	"github.com/open-policy-agent/opa/storage"
+	"github.com/open-policy-agent/opa/v1/storage"
 	admissionv1 "k8s.io/api/admission/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
