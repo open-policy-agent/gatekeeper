61c9d5cb4430601bb693e8e0814f05859fe7b146
Jaydip Gabani <gabanijaydip@gmail.com>
2025-11-22 00:51:56 +0000

    removing metadata.go
    Signed-off-by: Jaydip Gabani <gabanijaydip@gmail.com>

--- pkg/expansion/system.go
+++ pkg/expansion/system.go
@@ -251,6 +251,48 @@ func expandResource(obj *unstructured.Unstructured, ns *corev1.Namespace, templa
 	return resource, nil
 }
 
+// ensureOwnerReference appends an OwnerReference describing parent to the resultant
+// resource if one is not already present.
+func ensureOwnerReference(resultant, parent *unstructured.Unstructured) {
+	if resultant == nil || parent == nil {
+		return
+	}
+
+	parentAPIVersion := parent.GetAPIVersion()
+	parentKind := parent.GetKind()
+	parentName := parent.GetName()
+	if parentAPIVersion == "" || parentKind == "" || parentName == "" {
+		return
+	}
+
+	ownerRef := map[string]interface{}{
+		"apiVersion": parentAPIVersion,
+		"kind":       parentKind,
+		"name":       parentName,
+	}
+
+	refs, found, err := unstructured.NestedSlice(resultant.Object, "metadata", "ownerReferences")
+	if err != nil {
+		return
+	}
+	if found {
+		for _, ref := range refs {
+			refMap, ok := ref.(map[string]interface{})
+			if !ok {
+				continue
+			}
+			if refMap["apiVersion"] == parentAPIVersion && refMap["kind"] == parentKind && refMap["name"] == parentName {
+				return
+			}
+		}
+	} else {
+		refs = []interface{}{}
+	}
+
+	refs = append(refs, ownerRef)
+	_ = unstructured.SetNestedSlice(resultant.Object, refs, "metadata", "ownerReferences")
+}
+
 // mockNameForResource returns a mock name for a resultant resource created
 // from expanding `gen`. The name will be of the form:
 // "<generator name>-<resultant kind>". For example, a deployment named
