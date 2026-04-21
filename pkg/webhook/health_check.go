b67c9e93f619fb041d65d750c567e36fe919d62c
Sertac Ozercan <sozercan@gmail.com>
2026-04-21 14:36:15 -0700

    refactor: reuse TLS probe checker
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/webhook/health_check.go
+++ pkg/webhook/health_check.go
@@ -2,18 +2,22 @@ package webhook
 
 import (
 	"bytes"
+	"context"
 	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var tlsCheckerLog = logf.Log.WithName("webhook-tls-checker")
 
-func NewTLSChecker(certDir string, port int) func(*http.Request) error {
+func NewTLSChecker(certDir, host string, port int) func(*http.Request) error {
 	//nolint:forcetypeassert
 	tr := http.DefaultTransport.(*http.Transport).Clone()
 	// disabling gosec linting here as the http client used in this checking is intended to skip CA verification
@@ -24,9 +28,22 @@ func NewTLSChecker(certDir string, port int) func(*http.Request) error {
 	// fail if the cert was rotated in between
 	tr.DisableKeepAlives = true
 	insecureClient := &http.Client{Transport: tr}
+	probeURL := fmt.Sprintf("https://%s", net.JoinHostPort(tlsProbeHost(host), strconv.Itoa(port)))
 
-	returnFunc := func(_ *http.Request) error {
-		resp, err := insecureClient.Get(fmt.Sprintf("https://127.0.0.1:%d", port))
+	returnFunc := func(r *http.Request) error {
+		ctx := context.Background()
+		if r != nil {
+			ctx = r.Context()
+		}
+
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, probeURL, nil)
+		if err != nil {
+			newErr := fmt.Errorf("unable to create probe request: %w", err)
+			tlsCheckerLog.Error(newErr, "error creating https request for webhook server")
+			return newErr
+		}
+
+		resp, err := insecureClient.Do(req)
 		if err != nil {
 			newErr := fmt.Errorf("unable to connect to server: %w", err)
 			tlsCheckerLog.Error(newErr, "error in connecting to webhook server with https")
@@ -36,7 +53,7 @@ func NewTLSChecker(certDir string, port int) func(*http.Request) error {
 		// explicitly discard the body to avoid any memory leak
 		_, _ = io.Copy(io.Discard, resp.Body)
 
-		if len(resp.TLS.PeerCertificates) == 0 {
+		if resp.TLS == nil || len(resp.TLS.PeerCertificates) == 0 {
 			newErr := fmt.Errorf("webhook does not serve TLS certificate")
 			tlsCheckerLog.Error(newErr, "error in connecting to webhook server with https")
 			return newErr
@@ -67,4 +84,23 @@ func NewTLSChecker(certDir string, port int) func(*http.Request) error {
 		return nil
 	}
 	return returnFunc
+}
+
+func tlsProbeHost(host string) string {
+	if strings.HasPrefix(host, "[") && strings.HasSuffix(host, "]") {
+		host = strings.TrimPrefix(strings.TrimSuffix(host, "]"), "[")
+	}
+
+	if host == "" {
+		return "127.0.0.1"
+	}
+
+	if ip := net.ParseIP(host); ip != nil && ip.IsUnspecified() {
+		if ip.To4() != nil {
+			return "127.0.0.1"
+		}
+		return "::1"
+	}
+
+	return host
 }
