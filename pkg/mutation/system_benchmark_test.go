3ccc96b45f6154806bcfda8a21335633ae5c9612
Sertac Ozercan <sozercan@gmail.com>
2026-07-02 15:11:02 -0700

    perf: reduce mutation and sync hot-path overhead
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/mutation/system_benchmark_test.go
+++ pkg/mutation/system_benchmark_test.go
@@ -2,6 +2,7 @@ package mutation
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"github.com/open-policy-agent/gatekeeper/v3/apis/mutations/unversioned"
@@ -9,6 +10,7 @@ import (
 	"github.com/open-policy-agent/gatekeeper/v3/pkg/mutation/mutators"
 	"github.com/open-policy-agent/gatekeeper/v3/pkg/mutation/types"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func makeValue(v interface{}) unversioned.AssignField {
@@ -54,4 +56,50 @@ func BenchmarkSystem_Mutate(b *testing.B) {
 
 		_, _ = s.Mutate(context.Background(), &types.Mutable{Object: u})
 	}
+}
+
+func BenchmarkSystem_MutateNonMatchingMutatorsLargeObject(b *testing.B) {
+	s := NewSystem(SystemOpts{})
+	for i := 0; i < 100; i++ {
+		a := assign("", "spec.field"+strconv.Itoa(i))
+		a.Name = "assign-" + strconv.Itoa(i)
+		a.Spec.ApplyTo = []match.MutationApplyTo{{
+			ApplyTo: match.ApplyTo{
+				Groups:   []string{"apps"},
+				Versions: []string{"v1"},
+				Kinds:    []string{"Deployment"},
+			},
+		}}
+		m, err := mutators.MutatorForAssign(a)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if err := s.Upsert(m); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	base := &unstructured.Unstructured{Object: map[string]interface{}{
+		"apiVersion": "v1",
+		"kind":       "ConfigMap",
+		"metadata": map[string]interface{}{
+			"name":      "cm",
+			"namespace": "default",
+		},
+		"data": benchmarkLargeObjectData(500),
+	}}
+	base.SetGroupVersionKind(schema.GroupVersionKind{Group: "", Version: "v1", Kind: "ConfigMap"})
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = s.Mutate(context.Background(), &types.Mutable{Object: base})
+	}
+}
+
+func benchmarkLargeObjectData(entries int) map[string]interface{} {
+	data := make(map[string]interface{}, entries)
+	for i := 0; i < entries; i++ {
+		data["key"+strconv.Itoa(i)] = "value" + strconv.Itoa(i)
+	}
+	return data
 }
